ba38288746d259452014c6d14b2b214f33004c0e
Daniyal Ansari <daniyal.s.ansari+github@gmail.com>
2019-02-06 12:41:39 -0600

    Update tamulink-wpa.py
    Added comments. Not complete.

--- python/tamulink-wpa.py
+++ python/tamulink-wpa.py
@@ -1,16 +1,21 @@
 # run this file to connect to the WPA2 Enterprise wifi network
 
-import subprocess
-import socket
-import os
-import time
 
-user = os.getenv("SUDO_USER")
-if user is None:
-    print("\n   Execute as \033[1;31;48msudo\033[1;37;48m\n")
-    exit()
+# Import necessary libraries
 
-def internet(host="8.8.8.8", port=53, timeout=3):
+import subprocess	# Runs commands and gets output
+import socket		# Used to test internet connection
+import os		# Used to run system commands and checks if run as root user
+import time		# Used for delays
+
+# Check if the user that executed this program is root
+
+user = os.getenv("SUDO_USER")	
+if user is None:						# If not root
+    print("\n   Execute as \033[1;31;48msudo\033[1;37;48m\n")	# Prints request to run as root
+    exit()							# Closes program
+
+def internet(host="8.8.8.8", port=53, timeout=3):		# Function to check if internet connection is successful
     try:
         socket.setdefaulttimeout(timeout)
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
@@ -19,59 +24,60 @@ def internet(host="8.8.8.8", port=53, timeout=3):
         print(ex)
         return False
 
-search = "tamulink-wpa"
-ssid = search
+search = "tamulink-wpa"						# SSID to seach for tamulink-wpa. Will accept 
+ssid = search							# Also accepts parts of SSID like, "tam" or "link"
 
-scan = subprocess.Popen( ["connmanctl","services"], stdout=subprocess.PIPE)
-ssids = scan.stdout.readlines()
+scan = subprocess.Popen( ["connmanctl","services"], stdout=subprocess.PIPE)	# Run connmanctl services and get output
+ssids = scan.stdout.readlines()							# Break output into list named "ssids"
 
-print("\n\033[1;32;40mSearching For SSID:\033[1;37;40m", ssid)
+print("\n\033[1;32;40mSearching For SSID:\033[1;37;40m", ssid)	# Print SSID that is being searched for
 
-os.system("echo \"nameserver 8.8.8.8\" >> /etc/resolv.conf")
-
+os.system("echo \"nameserver 8.8.8.8\" >> /etc/resolv.conf")	# Writes "nameserver 8.8.8.8" to /etc/resolv.conf
+								# Tells Beaglebone what DNS server to use
 #try:
 
-while len(ssids) <= 1:
-	print("Scanning for more SSIDS")
-	subprocess.call("connmanctl"," scan"," wifi")
-	print("Have ", len(ssids), " ssids.")
+while len(ssids) <= 1:					# Checks how many SSIDs where found during scan
+	print("Scanning for more SSIDS")		# Scans again if no SSIDs were found
+	subprocess.call("connmanctl"," scan"," wifi")	# Runs system command to scan for more ssids
+	print("Have ", len(ssids), " ssids.")		# Prints number of SSIDs found after scan
 
-for x in range(len(ssids)):
-  ssids[x] = str(ssids[x])
+for x in range(len(ssids)):		# Looks at each SSID in list and converts each list entry to string	
+  ssids[x] = str(ssids[x])		# Convert to string
 
-for idx, val in enumerate(ssids):
-	if ssid in val:
-		ssid = val
+for idx, val in enumerate(ssids):	# Search through list for specified SSID or specified part of SSID
+	if ssid in val:			# If found SSID
+		ssid = val		# Replace specified SSID with found SSID
 
-ssid = ssid[0:len(ssid)-3].split()
-del ssid[0]
+ssid = ssid[0:len(ssid)-3].split()	# Split SSID string along space chars and store in list
+del ssid[0]				# Delete first entry in list because it's an artifact from
+					# bytes to sting in lines 44 and 45
+	
+print("\033[1;32;40m\nFound: \n\033[1;37;40m" + ssid[0] + "\t\t" + ssid[1])	# Output SSID found matching search string
 
-print("\033[1;32;40m\nFound: \n\033[1;37;40m" + ssid[0] + "\t\t" + ssid[1])
+username = input("\n\033[1;37;40mPlease enter your username: \033[0;37;40m")	# Ask for tamulink-wpa username
+password = input("\033[1;37;40mPlease enter your password: \033[0;37;40m")	# Ask fot tamulink-wpa password
 
-username = input("\n\033[1;37;40mPlease enter your username: \033[0;37;40m")
-password = input("\033[1;37;40mPlease enter your password: \033[0;37;40m")
+config_path = "/var/lib/connman/" + ssid[1] + ".config"		# Generate connmanctl wifi configuration file path and name
 
-config_path = "/var/lib/connman/" + ssid[1] + ".config"
+file = open(config_path,"w")	# Open file using generated name and path, will create if file is non-existant
 
-file = open(config_path,"w")
+print("\033[1;32;40m\nGenerated Configuration File Path: \n\033[1;37;40m" + config_path + "\n")		# Tell user configuration file name and path
 
-print("\033[1;32;40m\nGenerated Configuration File Path: \n\033[1;37;40m" + config_path + "\n")
+connmanctl_ssid = ssid[1].split("_")		# Split connmanctl wifi scan wireless details
 
-connmanctl_ssid = ssid[1].split("_")
+file.write("[service_"+ ssid[1] + "]\n")		# Use tamulink detailed ssid for file title
+file.write("Type = wifi\n")				# this is a WiFi file
+file.write("SSID = " + connmanctl_ssid[2] + "\n")	# Set SSID to detailed SSID
+file.write("EAP = peap\n")				# Set type of encapsulation
+file.write("Phase2 = MSCHAPV2\n")			# Set type of authentication for network
+file.write("Identity= " + username + "\n")		# Set network username
+file.write("Passphrase= " + password + "\n")		# Set network password
 
-file.write("[service_"+ ssid[1] + "]\n")
-file.write("Type = wifi\n")
-file.write("SSID = " + connmanctl_ssid[2] + "\n")
-file.write("EAP = peap\n")
-file.write("Phase2 = MSCHAPV2\n")
-file.write("Identity= " + username + "\n")
-file.write("Passphrase= " + password + "\n")
+file.close()						# Close file
 
-file.close()
-
-os.system("sudo systemctl restart connman")
-
-time.sleep(2)
+os.system("sudo systemctl restart connman")		# Restart connman service so it finds new configuration file
+							# It will automatically try to connect to network if it can see it
+time.sleep(2)						# 
 
 if internet() == True:
     print("\033[1;32;40m\nConnected!\n")
