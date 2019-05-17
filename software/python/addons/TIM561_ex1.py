fb31203cbbc6bde59563de0383022deeec9263a0
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-05-17 15:46:46 -0500

    Update TIM561_ex1.py

--- software/python/addons/TIM561_ex1.py
+++ software/python/addons/TIM561_ex1.py
@@ -1,4 +1,5 @@
 #Barebones program to grab data from the lidar unit.
+#library is located at http://www.github.com/ansarid/pysicktim
 
 # Import pysicktim Library as "lidar"
 import pysicktim as lidar
