e5a762f5edfeecc71fe7d89a74a914bb66da43f2
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-05-17 15:35:53 -0500

    update correct left / right

--- software/python/basics/encoder_ex2.py
+++ software/python/basics/encoder_ex2.py
@@ -1,4 +1,5 @@
 # Example to read left and right encoders.
+# Left has address 40 and right has 41
 # Code for Pi setup.
 
 import smbus
@@ -6,30 +7,31 @@ import time
 
 bus=smbus.SMBus(1)
 
-right_encoder = 0x40
-left_encoder  = 0x41
+left_encoder  = 0x40 # encoder i2c address
+right_encoder = 0x41 # encoder i2c address
 
-def read_encoders_angle(enc0,enc1):
+def read_encoders_angle(encL,encR):
     try:
-        x = bus.read_byte_data(enc0,0xFE)
+        x = bus.read_byte_data(encL,0xFE)
         x = ((x << 8) | (x >> 8)) & 0xFFFF
         meas = ((x & 0xFF00) >> 2) | ( x & 0x3F)
-        angle0 = meas*0.0219
+        angle0 = meas*0.0219 # convert to degrees
     except:
-        print('Warning (I2C): Could not read encoder0')
+        print('Warning (I2C): Could not read left encoder')
         angle0 = 0
     try:
-        x = bus.read_byte_data(enc1,0xFE)
+        x = bus.read_byte_data(encR,0xFE)
+        print
         x = ((x << 8) | (x >> 8)) & 0xFFFF
         meas = ((x & 0xFF00) >> 2) | ( x & 0x3F)
         angle1 = meas*0.0219 # convert to degrees
     except:
-        print('Warning (I2C): Could not read encoder1')
+        print('Warning (I2C): Could not read right encoder')
         angle1 = 0
     return [angle0, angle1]
 
 
 while 1:
-    x = read_encoders_angle(right_encoder,left_encoder)
-    print("Left: ",round(x[0],0),"\t","Right: ",round(x[1],0))
+    x = read_encoders_angle(left_encoder, right_encoder)
+    print("Left: ",round(x[0],3),"\t","Right: ",round(x[1],3))
     time.sleep(0.1)
