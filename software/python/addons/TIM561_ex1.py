756cf743b9ee80d070f45369a1b84c8b48995c3e
Daniyal Ansari <daniyal.s.ansari+github@gmail.com>
2019-05-17 13:28:14 -0500

    Update TIM561_ex1.py

--- software/python/addons/TIM561_ex1.py
+++ software/python/addons/TIM561_ex1.py
@@ -1,12 +1,13 @@
 #Barebones program to grab data from the lidar unit.
 
+# Import pysicktim Library as "lidar"
 import pysicktim as lidar
 
 # Repeat code nested in for loop 10 times
 for x in range(10):
 # while True:
 
-    lidar.scan()	# Requests and returns list of LIDAR
-        		# distance information in meters
+	lidar.scan()	# Requests and returns list of LIDAR
+					# distance information in meters
 
-    print(lidar.scan.distances)		# print distance list
+	print(lidar.scan.distances)		# print distance list
