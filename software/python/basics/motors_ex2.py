09d7dc2196352fccf9312e7e9c1630c23ea86d84
Daniyal Ansari <daniyal.s.ansari+github@gmail.com>
2019-05-20 15:11:31 -0500

    Update motors_ex2.py

--- software/python/basics/motors_ex2.py
+++ software/python/basics/motors_ex2.py
@@ -1,58 +1,44 @@
-# # A barebones example code to operate motors on the SCUTTLE robot.
-# # Uses Pi hardware.
-#
-# import RPi.GPIO as io
-# import time
-# io.setmode(io.BOARD)
-#
-# #        GPIO Define PWM, Direction
-#
-# dir_l = 11	# Left Motors Direction
-# dir_r = 13	# Right Motors Direction
-#
-# pwm_l_pin = 12	# Left Motors PWM - drives mot.driver pin
-# pwm_r_pin = 21	# Right Motors PWM
-#
-# #        Digital pwms Setup
-#
-# io.setup(dir_l,io.OUT)
-# io.setup(dir_r,io.OUT)
-#
-# #        Analog pwms Setup
-#
-# io.setup(pwm_l_pin, io.OUT)
-# io.setup(pwm_r_pin, io.OUT)
-#
-# pwm_l = io.PWM(pwm_l_pin, 1000)     # set Frequece to 1KHz
-# pwm_r = io.PWM(pwm_r_pin, 1000)     # set Frequece to 1KHz
-#
-#
-# while 1:
-#
-#     io.output(dir_r,0)
-#     io.output(dir_l,0)
-#
-#     pwm_l.ChangeDutyCycle(10)     # Change duty cycle percentage
-#     pwm_r.ChangeDutyCycle(0)     # Change duty cycle percentage
-
-#temporarily use an example "fade" program
-
 import time
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
 
-p = GPIO.PWM(12, 500)  # channel=12 frequency=50Hz
-p.start(0)
+r_wheel_pin = 32      # Right wheel
+l_wheel_pin = 33      # Left wheel
+
+r_dir_pin = 18
+l_dir_pin = 16
+
+r_wheel_dir = 0
+l_wheel_dir = 0
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(r_wheel_pin, GPIO.OUT)
+GPIO.setup(l_wheel_pin, GPIO.OUT)
+GPIO.setup(r_dir_pin, GPIO.OUT)
+GPIO.setup(l_dir_pin, GPIO.OUT)
+
+r_wheel = GPIO.PWM(r_wheel_pin, 500)  # channel=pin frequency=50Hz
+l_wheel = GPIO.PWM(l_wheel_pin, 500)  # channel=pin frequency=50Hz
+
+r_wheel.start(0)
+l_wheel.start(0)
+
 try:
+
     while 1:
-        for dc in range(0, 101, 5):
-            p.ChangeDutyCycle(dc)
-            time.sleep(0.1)
-        for dc in range(100, -1, -5):
-            p.ChangeDutyCycle(dc)
-            time.sleep(0.1)
+
+        GPIO.output(r_dir_pin, r_wheel_dir)
+        GPIO.output(l_dir_pin, l_wheel_dir)
+
+        r_wheel.ChangeDutyCycle(100)
+        l_wheel.ChangeDutyCycle(100)
+
+        time.sleep(0.1)
+
 except KeyboardInterrupt:
     pass
-p.stop()
+
+r_wheel.stop()
+l_wheel.stop()
+
 GPIO.cleanup()
