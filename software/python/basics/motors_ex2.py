1ab3c1d285fca5f7bf4203d3b052f436beb6bcb4
Daniyal Ansari <daniyal.s.ansari+github@gmail.com>
2019-05-20 15:14:11 -0500

    Update motors_ex2.py

--- software/python/basics/motors_ex2.py
+++ software/python/basics/motors_ex2.py
@@ -1,18 +1,18 @@
 import time
 import RPi.GPIO as GPIO
 
-r_wheel_pin = 32      # Right wheel
-l_wheel_pin = 33      # Left wheel
+r_wheel_pin = 32      # Right wheel pwm pin
+l_wheel_pin = 33      # Left wheel pwm pin
 
-r_dir_pin = 18
-l_dir_pin = 16
+r_dir_pin = 18        # Right Wheel direction pin
+l_dir_pin = 16        # Left Wheel direction pin
 
-r_wheel_dir = 0
+r_wheel_dir = 0     # 0 = LOW, 1 = HIGH
 l_wheel_dir = 0
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)    #Tells RPi how to interperet pin names
 
-GPIO.setup(r_wheel_pin, GPIO.OUT)
+GPIO.setup(r_wheel_pin, GPIO.OUT)   # Set outputs
 GPIO.setup(l_wheel_pin, GPIO.OUT)
 GPIO.setup(r_dir_pin, GPIO.OUT)
 GPIO.setup(l_dir_pin, GPIO.OUT)
@@ -20,17 +20,17 @@ GPIO.setup(l_dir_pin, GPIO.OUT)
 r_wheel = GPIO.PWM(r_wheel_pin, 500)  # channel=pin frequency=50Hz
 l_wheel = GPIO.PWM(l_wheel_pin, 500)  # channel=pin frequency=50Hz
 
-r_wheel.start(0)
+r_wheel.start(0)    # Start PWM at 0%
 l_wheel.start(0)
 
 try:
 
     while 1:
 
-        GPIO.output(r_dir_pin, r_wheel_dir)
+        GPIO.output(r_dir_pin, r_wheel_dir)     # Set digital pin
         GPIO.output(l_dir_pin, l_wheel_dir)
 
-        r_wheel.ChangeDutyCycle(100)
+        r_wheel.ChangeDutyCycle(100)     #Set PWM
         l_wheel.ChangeDutyCycle(100)
 
         time.sleep(0.1)
@@ -38,7 +38,7 @@ try:
 except KeyboardInterrupt:
     pass
 
-r_wheel.stop()
+r_wheel.stop()  # Stop PWM
 l_wheel.stop()
 
 GPIO.cleanup()
