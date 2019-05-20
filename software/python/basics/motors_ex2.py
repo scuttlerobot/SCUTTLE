cd5646f664df93a266f110626ca6f88779cd2065
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-05-20 15:25:25 -0500

    new version uses gpiozero library

--- software/python/basics/motors_ex2.py
+++ software/python/basics/motors_ex2.py
@@ -1,44 +1,49 @@
+import gpiozero
+from gpiozero import PWMOutputDevice
 import time
-import RPi.GPIO as GPIO
 
-r_wheel_pin = 32      # Right wheel pwm pin
-l_wheel_pin = 33      # Left wheel pwm pin
+#info on pins:
+# Broadcom (BCM) pin numbering for Pi gives names of GPIO17
+# and GPIO18 to physical pins 11 and 12
+leftOutA = PWMOutputDevice(17, frequency=1000,initial_value=0)
+leftOutB = PWMOutputDevice(18, frequency=1000,initial_value=0)
 
-r_dir_pin = 18        # Right Wheel direction pin
-l_dir_pin = 16        # Left Wheel direction pin
+RightOutA = PWMOutputDevice(22, frequency=1000,initial_value=0)
+RightOutB = PWMOutputDevice(23, frequency=1000,initial_value=0)
 
-r_wheel_dir = 0     # 0 = LOW, 1 = HIGH
-l_wheel_dir = 0
+leftOutA.frequency = 1000 #this is redundant
+leftOutA.value = 0.50 # this is for duty cycle
 
-GPIO.setmode(GPIO.BOARD)    #Tells RPi how to interperet pin names
+#channel refers to left(0) or right(1)
+def MotorL(speed):
+    if speed>0:
+        leftOutB.value = speed
+        leftOutA.value = 0
+    elif speed<0:
+        leftOutB.value = 0
+        leftOutA.value = (-1*speed) #drive opposite polarity with positive duty cycle
+    elif speed==0:
+        leftOutB.value = 0
+        leftOutA.value = 0
 
-GPIO.setup(r_wheel_pin, GPIO.OUT)   # Set outputs
-GPIO.setup(l_wheel_pin, GPIO.OUT)
-GPIO.setup(r_dir_pin, GPIO.OUT)
-GPIO.setup(l_dir_pin, GPIO.OUT)
-
-r_wheel = GPIO.PWM(r_wheel_pin, 500)  # channel=pin frequency=50Hz
-l_wheel = GPIO.PWM(l_wheel_pin, 500)  # channel=pin frequency=50Hz
-
-r_wheel.start(0)    # Start PWM at 0%
-l_wheel.start(0)
+def MotorR(speed):
+    if speed>0:
+        RightOutB.value = speed
+        RightOutA.value = 0
+    elif speed<0:
+        RightOutB.value = 0
+        RightOutA.value = (-1*speed) #drive opposite polarity with positive duty cycle
+    elif speed==0:
+        RightOutB.value = 0
+        RightOutA.value = 0
 
 try:
-
     while 1:
-
-        GPIO.output(r_dir_pin, r_wheel_dir)     # Set digital pin
-        GPIO.output(l_dir_pin, l_wheel_dir)
-
-        r_wheel.ChangeDutyCycle(100)     #Set PWM
-        l_wheel.ChangeDutyCycle(100)
-
-        time.sleep(0.1)
-
+        MotorL(0.5)
+        #MotorR(0.5)
+        time.sleep(4)
+        MotorL(-0.5)
+        #MotorR(-0.6)
+        time.sleep(4)
 except KeyboardInterrupt:
     pass
-
-r_wheel.stop()  # Stop PWM
-l_wheel.stop()
-
-GPIO.cleanup()
