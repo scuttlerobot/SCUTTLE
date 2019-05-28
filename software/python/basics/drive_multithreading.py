a4a0b86a11786096ece8b70f220e337a830d95c0
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-05-28 16:36:05 -0500

    Update drive_multithreading.py

--- software/python/basics/drive_multithreading.py
+++ software/python/basics/drive_multithreading.py
@@ -1,31 +1,35 @@
-# this program currently features:
-# importing of other our 'basic' python files and calling functions from those
-# performing multithreading with two loops (loop_speak and loop_drive)
-# a crude but simplified mapping of the gamepad axes 0 and 1 to the driving.
-# reciting strings upon button presses (a,x,b on gamepad)
-# generate_duty is intended to be replaced by a kinematics program PLUS pid controller program.
-
-# last significant update: 2019.05.24
+# this program demonstrates importing of other python files and
+# calling functions from child files.
+# last updated 2019.05.23
 
 import motors_ex2 as m #module calculates PWM commands
 import encoder_ex2 as enc # for encoders
-import gamepad_ex2 as gp #for gamepad
 import time
 import numpy as np # for handling matrices
 import text2speech_ex2 as t2s #for speaking by aux port
 import threading # only used for threading functions
+import speed_control as sc # for generating speed commands
+import inverse_kinematics as inv
+import gamepad_ex2 as gp # TEMPORARILY IMPORT THIS FILE ONLY!
 
 m.MotorL(0)
 m.MotorR(0)
 axes = np.zeros(16) #number of elements returned by gamepad
 
-# generate_duty is a placeholder function for the inverse kinematics program
-def generate_duty(x_dot,theta_dot):
-    # Calculate Left and Right Wheel Duty Cycles
-    duty_r = ((  1 * (theta_dot)) + x_dot )
-    duty_l = (( -1 * (theta_dot))  + x_dot )
-    duties = np.array([ duty_l, duty_r ])
-    return duties
+#this function to be used temporarily to offer speeds in [-1,1] range to m.drive fcn
+def populate_gp_temporary():
+    gpData = gp.getGP() #when there is no controller input, update is empty
+    try:                      # when update has no data, update.size DNE
+        if gpData.size == 16:  # if update has data, store it to axes
+            axes = gpData # now axes is a 16-element array
+    except:
+        pass
+    # assign axes from gamepad to requested velocities
+    x_dot = -1*axes[1] # assign forward axis, inverted
+    theta_dot = -1*axes[0] # assign L/R axis, inverted
+    B_raw = np.array([x_dot, theta_dot]) # form the B matrix
+    return(B_raw)
+
 
 def loop_speak( ID ):
     while(1):
@@ -43,26 +47,14 @@ def loop_speak( ID ):
 
 def loop_drive( ID ):
     while(1):
-            # verify motors are working
-            update = gp.getGP() #when there is no controller input, update is empty
-            try:                      # when update has no data, update.size DNE
-                if update.size == 16:  # if update has data, store it to axes
-                    axes = update
-                    print("update size :", update.size)
-            except:
-                pass
-
-            # assign axes grom gamepad to requested velocities
-            x_dot = -1*axes[1] # times -1 so forward gives positive
-            theta_dot = -1*axes[0]
-            #generate duty cycles
-            duties = generate_duty(x_dot, theta_dot)
-            duties[0] = sorted([-1, duties[0], 1])[1] # place bounds on duty cycle
-            duties[1] = sorted([-1, duties[1], 1])[1] # place bounds on duty cycle
-            print("x dot: ", x_dot)
-            m.MotorL(duties[0]) # fcn allows -1 to 1
-            m.MotorR(duties[1]) # fcn allows -1 to 1
-
+            #get the latest target phi_dots from inverse kinematics
+            phis = inv.get_phis()
+            print("phi dot left:", phis[0])
+            #call the speed control system to action:
+            xt = populate_gp_temporary()
+            x = xt[0]
+            t = xt[1]
+            sc.drive(x,t)
             time.sleep(0.1)
 
 
