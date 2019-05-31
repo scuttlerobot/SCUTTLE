2ec51b89fd7382353df361cc7953cec70283fc40
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-05-31 16:57:29 -0500

    add phi_influence for repel obstacles

--- software/python/basics/drive_multithreading.py
+++ software/python/basics/drive_multithreading.py
@@ -12,14 +12,17 @@ import encoder_ex2 as enc # for encoders
 import speed_control as sc # for generating speed commands
 import inverse_kinematics as inv
 import gamepad_ex2 as gp # TEMPORARILY IMPORT THIS FILE ONLY!
+import kinematics as kin
+import repel as repel
 
 axes = np.zeros(16) #number of elements returned by gamepad
 
 def loop_speak( ID ):
     while(1):
-        myStringA = "hello everybody I'm a multi-threading scuttle"
-        myStringX = "you pressed X!"
-        myStringB = "firing the missiles"
+        myStringA = "hello everybody, I am scuttle robot"
+        myStringX = "do you smell what the rock is cooking?"
+        myStringB = "Special delivery"
+        myStringY = "gig em aggieees"
         signals = gp.getGP()
         if signals[6]==1: # A button is pressed
             t2s.say(myStringA)
@@ -27,17 +30,32 @@ def loop_speak( ID ):
             t2s.say(myStringX)
         if signals[5]==1: # x button is pressed
             t2s.say(myStringB)
-        time.sleep(0.1)
+        if signals[4]==1: #
+            t2s.say(myStringY)
+        time.sleep(0.05)
 
 def loop_drive( ID ):
     while(1):
             #get the latest target phi_dots from inverse kinematics
             phis = inv.get_phis() # populates target phi dots, targets
-            print("phi dot left:", phis[0])
+            #print("phi dot left:", phis[0])
+
+            d = repel.nearest_point() #returns the distance and the y-value of nearest obstacle
+            print("nearest point:", d)
+            phi_influence = inv.phi_influence(d[1]) #send the y-value of nearest point to this fcn.
+            phis = phis + phi_influence #combine the obstacle influence with phis
+            
             #call the speed control system to action:
             sc.driveOpenLoop(phis[0],phis[1])
             time.sleep(0.1)
+            motion = kin.getMotion() # populate thetaDot & xDot
+            #print("thetadot,xdot", motion)
 
+def loop_scan( ID ):
+    while(1):
+            # d = repel.nearest_point() #returns the distance and the y-value of nearest obstacle
+            # print("nearest point:", d)
+            time.sleep(0.1)
 
 def main():
         print("starting the main fcn")
@@ -50,7 +68,12 @@ def main():
         threads.append(t2)
         t2.start()
         print("started thread2")
+        t3 = threading.Thread( target=loop_scan, args=(3,) ) # make 2nd thread object
+        threads.append(t3)
+        t3.start()
+        print("started thread3")
+
         t.join()
         t2.join()
-
+        t3.join()
 main()
