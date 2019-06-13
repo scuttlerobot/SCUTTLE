46e36e3921e47a8b889e5f74a088bbf89e9ac3fc
ansarid <daniyal.s.ansari@gmail.com>
2019-06-13 03:21:58 -0500

    Commented and improved code

--- software/python/remote_console/dashboard_server.py
+++ software/python/remote_console/dashboard_server.py
@@ -1,18 +1,32 @@
-import socket
 import time
 import json
-import sys
+import socket
 
-ip = "0.0.0.0"
-port = 2442
+def current_time(start_time):
+    current_time = time.time() - start_time
+    day = current_time // (24 * 3600)
+    current_time = current_time % (24 * 3600)
+    hour = current_time // 3600
+    current_time %= 3600
+    minutes = current_time // 60
+    current_time %= 60
+    seconds = round(current_time,3)
+    return hour, minutes, seconds
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def log(*args):
+    t = current_time(start_time)
+    print("%02d:%02d:%02.3f -" % (t[0],t[1],t[2]),' '.join(map(str, args)))
+
+ip = "0.0.0.0"      # 0.0.0.0 will make the server accessable on all network interfaces
+port = 2442         # Port to run server on
+
+start_time = time.time()    # Record start time for logging
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)   # Setup UDP Communication
 socket.bind((ip, port))
 
 print("Bound to IP:  ",ip,"\n\t Port:",port)
 print("\nServer running!")
-
-i = 0
 
 while 1:
 
@@ -22,36 +36,36 @@ while 1:
       "c": 3
     }
 
+######################################
+
+    # UPDATE DATA HERE
+
+    # Example:
+    # data["batt_v"] = analog_read(battery)
+
+######################################
 
     try:
 
-        request, ip = socket.recvfrom(1024)
-        print(i," - Received Request from", ip[0])
-        request = json.loads(request.decode('utf-8'))
-        packet = []
+        request, ip = socket.recvfrom(1024)                     # Wait until data is received
+        request = json.loads(request.decode('utf-8'))           # Converts data back from bytes to string
+        log("Received Request from", ip[0], "for:", request)    # Log to console
+        packet = []                                             # Create empty list to construct packet
 
-        for item in request:
+        for item in request:                # Iterate through requested items and
+                                            # assemble packet in order requested
+            if item in data:                # Check requested item exists in data dictionary
 
-            if item in data:
+                packet.append(data[item])   # If items exists append to end of packet
 
-                packet.append(data[item])
+            elif item not in data:                              # If item doesnt exist in data dictionary
+                log("Item \"",item,"\"", "does not exist!")     # Log to console
+                packet.append(None)                             # append 'None' for better error handling
 
-            elif item not in data:
+        packet = json.dumps(packet)                     # Convert message to bytes
+        socket.sendto(packet.encode('utf-8'), ip)       # Send back to device that requested
+        log("Sent response", packet,"to",ip[0])         # Log to console
 
-                packet.append(None)
-
-        packet = json.dumps(packet)
-        socket.sendto(packet.encode(), ip)
-
-    except Exception as e:
-
-        if e == "KeyboardInterrupt":
-
-            print("Exiting!")
-            exit()
-
-        else:
-
-            print(e)
-
-    i+=1
+    except Exception as e:      # If there is an error
+        print(e)                # Print error
+        exit()                  # Exit code. Replace with "pass" for code to move on after error
