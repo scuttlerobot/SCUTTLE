ee3f13a410a9d396eb58d177bf57d2242ac79d4b
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-06-24 09:11:01 -0500

    Update motors_ex1.py

--- software/python/basics/motors_ex1.py
+++ software/python/basics/motors_ex1.py
@@ -1,19 +1,29 @@
-#This example sets the motors to full speed forward.
+# This example drives the right and left motors.
+# Intended for Beagle hardware
 
 import rcpy
 import rcpy.motor as motor
+import time # only necessary if running this program as a loop
 
 motor_r = 2 	# Right Motor
 motor_l = 1 	# Left Motor
-
 rcpy.set_state(rcpy.RUNNING)
 
+#channel refers to left(0) or right(1)
+def MotorL(speed):
+    motor.set(motor_l, duty_l)
+    
+def MotorR(speed):
+    motor.set(motor_r, duty_r)
+
+# Uncomment this section to run this program as a standalone loop
 while rcpy.get_state() != rcpy.EXITING:
 
     if rcpy.get_state() == rcpy.RUNNING:
 
-        duty_l = 1
-        duty_r = 1
-
-        motor.set(motor_l, duty_l)
-        motor.set(motor_r, duty_r)
+        MotorL(0.3)  # gentle speed for testing program
+        MotorR(0.3)
+        time.sleep(2) # run fwd for 2 seconds
+        MotorL(-0.3)
+        MotorR(-0.3) 
+        time.sleep(2) # run reverse for 2 seconds
