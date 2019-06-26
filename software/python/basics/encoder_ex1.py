f6fb6c538f2fb905db3a440fb00a2071bd8ca1a3
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-06-26 12:26:20 -0500

    update to agree with architecture 06.26

--- software/python/basics/encoder_ex1.py
+++ software/python/basics/encoder_ex1.py
@@ -1,14 +1,18 @@
 #This example reads left and right encoders and outputs the position (deg) to the terminal
+# Left has address 40 and right has 41
+# Code for Beagle Hardware
 
 import Adafruit_GPIO.I2C as Adafruit_I2C
 import time
+import numpy as np
 
-right_encoder = Adafruit_I2C.Device(0x40,1)
-left_encoder  = Adafruit_I2C.Device(0x41,1)
 
-def read_encoders_angle(enc0,enc1):
+encL = Adafruit_I2C.Device(0x40,1) # encoder i2c address
+encR  = Adafruit_I2C.Device(0x41,1) # encoder i2c address
+
+def read():
     try:
-        x = enc0.readU16(0xFE)
+        x = encL.readU16(0xFE)
         x = ((x << 8) | (x >> 8)) & 0xFFFF
         meas = ((x & 0xFF00) >> 2) | ( x & 0x3F)
         angle0 = meas*0.0219
@@ -16,17 +20,23 @@ def read_encoders_angle(enc0,enc1):
         print('Warning (I2C): Could not read encoder0')
         angle0 = 0
     try:
-        x = enc1.readU16(0xFE)
+        x = encR.readU16(0xFE)
         x = ((x << 8) | (x >> 8)) & 0xFFFF
         meas = ((x & 0xFF00) >> 2) | ( x & 0x3F)
         angle1 = meas*0.0219 # convert to degrees
     except:
         print('Warning (I2C): Could not read encoder1')
         angle1 = 0
-    return [angle0, angle1]
+    angles = np.array([angle0,angle1])
+    return angles
 
 
-while True:
-    x = read_encoders_angle(right_encoder,left_encoder)
-    print("Left: ",round(x[0],0),"\t","Right: ",round(x[1],0))
-    time.sleep(0.1)
+
+# UNCOMMENT THIS SECTION TO USE ENCODER_EX2.PY AS A STANDALONE PROGRAM
+# ------------------------------------------------------------------------------
+# while 1:
+#     encValues = read()
+#     # round the values and print them separated by a tab
+#     print("Left: ",round(encValues[0],1),"\t","Right: ",round(encValues[1],1))
+
+#     time.sleep(0.1)
