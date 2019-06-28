c6d4a95547b80c4add5f736214f9922d397d49c0
RMC-800 <50638575+RMC-800@users.noreply.github.com>
2019-06-28 11:29:00 -0500

    Update motors_ex1.py

--- software/python/basics/motors_ex1.py
+++ software/python/basics/motors_ex1.py
@@ -12,18 +12,19 @@ rcpy.set_state(rcpy.RUNNING)
 #channel refers to left(0) or right(1)
 def MotorL(speed):
     motor.set(motor_l, speed)
-    
+
 def MotorR(speed):
     motor.set(motor_r, speed)
 
 # Uncomment this section to run this program as a standalone loop
-while rcpy.get_state() != rcpy.EXITING:
+# while rcpy.get_state() != rcpy.EXITING:
+#
+#     if rcpy.get_state() == rcpy.RUNNING:
+#
+#         MotorL(0.5)  # gentle speed for testing program. 0.3 PWM may not spin wheels.
+#         MotorR(0.5)
+#         time.sleep(4) # run fwd for 4 seconds
+#         MotorL(-0.5)
+#         MotorR(-0.5)
+#         time.sleep(2) # run reverse for 2 seconds
 
-    if rcpy.get_state() == rcpy.RUNNING:
-
-        MotorL(0.5)  # gentle speed for testing program. 0.3 PWM may not spin wheels.
-        MotorR(0.5)
-        time.sleep(4) # run fwd for 4 seconds
-        MotorL(-0.5)
-        MotorR(-0.5) 
-        time.sleep(2) # run reverse for 2 seconds
