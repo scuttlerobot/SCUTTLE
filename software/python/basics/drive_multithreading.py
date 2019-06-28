e7d4c98d1e8295c7e59531ebf95ced8fe6bd4468
RMC-800 <50638575+RMC-800@users.noreply.github.com>
2019-06-28 10:26:43 -0500

    Update drive_multithreading.py
    optimized to drive on beagle

--- software/python/basics/drive_multithreading.py
+++ software/python/basics/drive_multithreading.py
@@ -5,33 +5,33 @@
 # IMPORT EXTERNAL ITEMS
 import time
 import numpy as np # for handling matrices
-import text2speech_ex2 as t2s #for speaking by aux port
+# import text2speech_ex2 as t2s #for speaking by aux port
 import threading # only used for threading functions
-# IMPORT INTERNAL ITEMS
-import encoder_ex2 as enc # for encoders
+# # IMPORT INTERNAL ITEMS
+import encoder_ex1 as enc # for encoders
 import speed_control as sc # for generating speed commands
 import inverse_kinematics as inv
-import gamepad_ex2 as gp # TEMPORARILY IMPORT THIS FILE ONLY!
+import gamepad_ex1 as gp # TEMPORARILY IMPORT THIS FILE ONLY!
 import kinematics as kin
-import repel as repel
+# import repel as repel
 
 axes = np.zeros(16) #number of elements returned by gamepad
 
 def loop_speak( ID ):
     while(1):
-        myStringA = "I am scuttle robot"
-        myStringX = "the future of meka tronics"
-        myStringB = "Special delivery"
-        myStringY = "gig em aggies"
-        signals = gp.getGP()
-        if signals[6]==1: # A button is pressed
-            t2s.say(myStringA)
-        if signals[7]==1: # x button is pressed
-            t2s.say(myStringX)
-        if signals[5]==1: # x button is pressed
-            t2s.say(myStringB)
-        if signals[4]==1: #
-            t2s.say(myStringY)
+        # myStringA = "I am scuttle robot"
+        # myStringX = "the future of meka tronics"
+        # myStringB = "Special delivery"
+        # myStringY = "gig em aggies"
+        # signals = gp.getGP()
+        # if signals[6]==1: # A button is pressed
+        #     t2s.say(myStringA)
+        # if signals[7]==1: # x button is pressed
+        #     t2s.say(myStringX)
+        # if signals[5]==1: # x button is pressed
+        #     t2s.say(myStringB)
+        # if signals[4]==1: #
+        #     t2s.say(myStringY)
         time.sleep(0.05)
 
 def loop_drive( ID ):
@@ -56,10 +56,10 @@ def loop_drive( ID ):
 
 def loop_scan( ID ):
     while(1):
-            d = repel.nearest_point() #returns the distance and the y-value of nearest obstacle
-            #print("nearest point:", d)
-            if d[0] < 0.15: #if the nearest obstacle less than 10cm
-                    t2s.say("NO")
+            # d = repel.nearest_point() #returns the distance and the y-value of nearest obstacle
+            # #print("nearest point:", d)
+            # if d[0] < 0.15: #if the nearest obstacle less than 10cm
+            #         t2s.say("NO")
             time.sleep(0.1)
 
 def main():
