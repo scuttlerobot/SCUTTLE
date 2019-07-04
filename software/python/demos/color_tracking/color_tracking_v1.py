ca39a17a7ad228eaefee5669ec48c1c9ab13651d
Daniyal Ansari <daniyal@ThinkPad-T420>
2019-07-04 13:30:22 -0500

    commented color_tracking

--- software/python/demos/color_tracking/color_tracking_v1.py
+++ software/python/demos/color_tracking/color_tracking_v1.py
@@ -3,51 +3,46 @@
 # This program was designed to have SCUTTLE following a basketball.
 # The calibration was made in a brightly lit indoor environment.
 # Video demo: https://youtu.be/9t1XHcomlIs
-
+color Tracking
 print("loading libraries for color tracking...")
-import cv2
-import argparse
-import numpy as np
-import os
-import time
-
+import cv2              # For image capture and processing
+import argparse         # For fetching user arguments
+import numpy as np      # Kernel
 
 print("loading rcpy.")
-# import rcpy library
-# This automatically initizalizes the robotics cape
-import rcpy
-import rcpy.motor as motor
+import rcpy                 # Import rcpy library
+import rcpy.motor as motor  # Import rcpy motor module
 print("finished loading libraries.")
 #    Camera
 
-camera_input = 0
+camera_input = 0        # Define camera input. Default=0. 0=/dev/video0
 
-size_w  = 240   #this is the pixel width
-size_h = 160	#this is the pixel height
+size_w  = 240   # Resized image width. This is the image width in pixels.
+size_h = 160	# Resized image height. This is the image height in pixels.
 
 #    Color Range, described in HSV
 
-v1_min = 30
-v2_min = 20
-v3_min = 245
+v1_min = 30     # Minimum H value
+v2_min = 20     # Minimum S value
+v3_min = 245    # Minimum V value
 
-v1_max = 43
-v2_max = 98
-v3_max = 255
+v1_max = 43     # Maximum H value
+v2_max = 98     # Maximum S value
+v3_max = 255    # Maximum V value
 
 #    RGB or HSV
 
-filter = 'HSV'
+filter = 'HSV'  # Use HSV to describe pixel color values
 
 def main():
 
-    camera = cv2.VideoCapture(camera_input)
-    camera.set(3, size_w)
-    camera.set(4, size_h)
+    camera = cv2.VideoCapture(camera_input)     # Define camera variable
+    camera.set(3, size_w)                       # Set width of images that will be retrived from camera
+    camera.set(4, size_h)                       # Set height of images that will be retrived from camera
 
-    tc = 70     # Too Close
-    tf = 6      # Too Far
-    tp = 65     # Target Pixels
+    tc = 70     # Too Close     - Maximum pixel size of object to track
+    tf = 6      # Too Far       - Minimum pixel size of object to track
+    tp = 65     # Target Pixels - Target size of object to track
 
     band = 50   #range of x considered to be centered
 
@@ -60,7 +55,7 @@ def main():
     duty_r = 0 # initialize motor with zero duty cycle
 
     print("initializing rcpy...")
-    rcpy.set_state(rcpy.RUNNING)
+    rcpy.set_state(rcpy.RUNNING)        # initialize rcpy
     print("finished initializing rcpy.")
 
     try:
@@ -75,40 +70,40 @@ def main():
                 motor_r = 2 	# Right Motor assigned to #2
                 motor_l = 1 	# Left Motor assigned to #1
 
-                ret, image = camera.read()
+                ret, image = camera.read()  # Get image from camera
 
-                height, width, channels = image.shape
+                height, width, channels = image.shape   # Get size of image
 
                 if not ret:
                     break
 
-                if filter == 'RGB':
+                if filter == 'RGB':                     # If image mode is RGB switch to RGB mode
                     frame_to_thresh = image.copy()
                 else:
-                    frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+                    frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)    # Otherwise continue reading in HSV
 
-                thresh = cv2.inRange(frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))
+                thresh = cv2.inRange(frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))   # Find all pixels in color range
 
-                kernel = np.ones((5,5),np.uint8)
-                mask = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+                kernel = np.ones((5,5),np.uint8)                            # Set gaussian blur strength.
+                mask = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)     # Apply gaussian blur
                 mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
-                cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
-                center = None
+                cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]     # Find closed shapes in image
+                center = None   # Create variable to store point
 
-                if len(cnts) > 0:
+                if len(cnts) > 0:   # If more than 0 closed shapes exist
 
-                    c = max(cnts, key=cv2.contourArea)
-                    ((x, y), radius) = cv2.minEnclosingCircle(c)
+                    c = max(cnts, key=cv2.contourArea)              # Get the properties of the largest circle
+                    ((x, y), radius) = cv2.minEnclosingCircle(c)    # Get properties of circle around shape
 
-                    radius = round(radius, 2)
+                    radius = round(radius, 2)   # Round radius value to 2 decimals
 
-                    x = int(x)
-                    M = cv2.moments(c)
-                    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+                    x = int(x)          # Cast x value to an integer
+                    M = cv2.moments(c)  # Gets area of circle contour
+                    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))   # Get center x,y value of circle
 
                     # handle centered condition
-                    if x > ((width/2)-(band/2)) and x < ((width/2)+(band/2)):
+                    if x > ((width/2)-(band/2)) and x < ((width/2)+(band/2)):       # If center point is centered
 
                         dir = "driving"
 
@@ -132,11 +127,13 @@ def main():
                     else:
                         case = "turning"
 
-                        duty_l = round((x-0.5*width)/(0.5*width),2)       # turn left
+                        duty_l = round((x-0.5*width)/(0.5*width),2)     # Duty Left
                         duty_l = duty_l*scale_t
 
-                        duty_r = round((0.5*width-x)/(0.5*width),2)
+                        duty_r = round((0.5*width-x)/(0.5*width),2)     # Duty Right
                         duty_r = duty_r*scale_t
+
+                    # Keep duty cycle within range
 
                     if duty_r > 1:
                         duty_r = 1
@@ -150,14 +147,17 @@ def main():
                     elif duty_l < -1:
                         duty_l = -1
 
+                    # Round duty cycles
                     duty_l = round(duty_l,2)
                     duty_r = round(duty_r,2)
 
                     print(case, "\tradius: ", round(radius,1), "\tx: ", round(x,0), "\t\tL: ", duty_l, "\tR: ", duty_r)
 
+                    # Set motor duty cycles
                     motor.set(motor_l, duty_l)
                     motor.set(motor_r, duty_r)
 
+                # Set motor duty cycles
                 motor.set(motor_l, duty_l)
                 motor.set(motor_r, duty_r)
 
@@ -172,7 +172,6 @@ def main():
 
     	rcpy.set_state(rcpy.EXITING)
     	print("Exiting Color Tracking.")
-
 
 # exiting program will automatically clean up cape
 
