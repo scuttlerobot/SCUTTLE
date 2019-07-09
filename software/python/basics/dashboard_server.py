c19b45abc63348ab70ebf0a6654af19fa4be7775
RMC-800 <50638575+RMC-800@users.noreply.github.com>
2019-07-09 09:31:06 -0500

    Update dashboard_server.py

--- software/python/basics/dashboard_server.py
+++ software/python/basics/dashboard_server.py
@@ -1,49 +1,86 @@
-import socket
 import time
 import json
-import kinematics as k
+import socket
+###############################
+import encoder_ex1 as enc
+import speed_control as sc
+import inverse_kinematics as inv
+import gamepad_ex1 as gp
+import kinematics as kin
+###############################
 
-port = 9999
+def current_time(start_time):
+    current_time = time.time() - start_time
+    day = current_time // (24 * 3600)
+    current_time = current_time % (24 * 3600)
+    hour = current_time // 3600
+    current_time %= 3600
+    minutes = current_time // 60
+    current_time %= 60
+    seconds = round(current_time,3)
+    return hour, minutes, seconds
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# socket = sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def log(*args):
+    t = current_time(start_time)
+    print("%02d:%02d:%02.3f -" % (t[0],t[1],t[2]),' '.join(map(str, args)))
 
-# socket.bind(("", port))
-socket.bind(("localhost", port))
+ip = "0.0.0.0"      # 0.0.0.0 will make the server accessable on all network interfaces
+#port = 2442         # Port to run server on
+port = 2222
+#port = 135
 
-print("Server running!")
+start_time = time.time()    # Record start time for logging
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)   # Setup UDP Communication
+socket.bind((ip, port))
+
+print("Bound to IP:  ",ip,"\n\t Port:",port)
+print("\nServer running!")
 
 while 1:
 
-    theta = k.getMotion()
+    data = {
+      "targ_phi_L": 1,
+      "targ_phi_R": 2,
+      "curr_phi_L": 3,
+      "curr_phi_R": 4,
+      "x":[10,10,10,10,10,10,10,10,10,10],
+      "y":[1,2,3,4,5,6,7,8,9,9.7],
+      "phi_dots_L":None,
+      "phi_dots_R":None
+    }
 
-    data = {"thetaDot": theta[0],
-            "xDot": theta[1]
-            }
+######################################
+
+    # UPDATE DATA HERE
+
+    # Example:
+    PhiDots = kin.getPhiDots()
+    data["phi_dots_L"] = PhiDots[0]
+    data["phi_dots_R"] = PhiDots[1]
+######################################
 
     try:
 
-        request, ip = socket.recvfrom(1024)
-        request = json.loads(request)
+        request, ip = socket.recvfrom(1024)                     # Wait until data is received
+        request = json.loads(request.decode('utf-8'))           # Converts data back from bytes to string
+        log("Received Request from", ip[0], "for:", request)    # Log to console
+        packet = []                                             # Create empty list to construct packet
 
-        packet = []
+        for item in request:                # Iterate through requested items and
+                                            # assemble packet in order requested
+            if item in data:                # Check requested item exists in data dictionary
 
-        for item in request:
+                packet.append(data[item])   # If items exists append to end of packet
 
-            if item in data:
+            elif item not in data:                              # If item doesnt exist in data dictionary
+                log("Item \"",item,"\"", "does not exist!")     # Log to console
+                packet.append(None)                             # append 'None' for better error handling
 
-                packet.append(data[item])
+        packet = json.dumps(packet)                     # Convert message to bytes
+        socket.sendto(packet.encode('utf-8'), ip)       # Send back to device that requested
+        log("Sent response", packet,"to",ip[0])         # Log to console
 
-            elif item not in data:
-
-                packet.append(None)
-
-        print(packet)
-
-        packet = json.dumps(packet)
-
-        socket.sendto(packet.encode(), ip)
-
-    except:
-
-        pass
+    except Exception as e:      # If there is an error
+        print(e)                # Print error
+        exit()                  # Exit code. Replace with "pass" for code to move on after error
