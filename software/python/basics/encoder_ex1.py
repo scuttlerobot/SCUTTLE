7bd69a9cffabb01e6957cded1a1ad9ad075b58ed
RMC-800 <50638575+RMC-800@users.noreply.github.com>
2019-07-16 15:15:10 -0500

    Update encoder_ex1.py

--- software/python/basics/encoder_ex1.py
+++ software/python/basics/encoder_ex1.py
@@ -6,37 +6,42 @@ import Adafruit_GPIO.I2C as Adafruit_I2C
 import time
 import numpy as np
 
-
 encL = Adafruit_I2C.Device(0x42,1) # encoder i2c address
 encR  = Adafruit_I2C.Device(0x41,1) # encoder i2c address
-
-def read():
+def read(channel):
     try:
-        x = encL.readU16(0xFE)
+        # The AS5048B encoder gives a 14 bit angular reading
+        if channel == 'L':
+            x = encL.readU16(0xFE) # capture the 8 msb's from encoder
+            y = encL.readU16(0xFF) # capture the 6 lsb's from encoder
+        elif channel == "R":
+            x = encR.readU16(0xFE) # capture the 8 msb's from encoder
+            y = encR.readU16(0xFF) # capture the 6 lsb's from encoder
+        y = y * 0.0219  # convert y directly to degrees.
+        # y can contribute 1.4 degrees to the reading at most
+        # for x, perform bitwise operation to get true scaling of these bits
         x = ((x << 8) | (x >> 8)) & 0xFFFF
-        meas = ((x & 0xFF00) >> 2) | ( x & 0x3F)
-        angle0 = meas*0.0219
+        x = ((x & 0xFF00) >> 2) | ( x & 0x3F)
+        # x value now ready to convert to degrees
+        x = x*0.0219
+        # add the x and y values to get the full measurement
+        angle = x + y
     except:
         print('Warning (I2C): Could not read encoder0')
-        angle0 = 0
-    try:
-        x = encR.readU16(0xFE)
-        x = ((x << 8) | (x >> 8)) & 0xFFFF
-        meas = ((x & 0xFF00) >> 2) | ( x & 0x3F)
-        angle1 = meas*0.0219 # convert to degrees
-    except:
-        print('Warning (I2C): Could not read encoder1')
-        angle1 = 0
-    angles = np.array([angle0,angle1])
-    return angles
+        angle = 0
+    return angle
 
-
+#the second function returns the encoder values that will be used in the rest of the programs
+def readEncs():
+    encLeft = round(read('L'),1)
+    encRight = round(read('R'),1)
+    encoders = np.array([encLeft,encRight])
+    return encoders
 
 # UNCOMMENT THIS SECTION TO USE ENCODER_EX2.PY AS A STANDALONE PROGRAM
 # ------------------------------------------------------------------------------
 # while 1:
-#       encValues = read()
-# #      round the values and print them separated by a tab
-#       print("Left: ",round(encValues[0],1),"\t","Right: ",round(encValues[1],1))
-# #
+#       encoders = readEncs()
+#       # round the values and print them
+#       print("encoders: ", encoders)
 #       time.sleep(0.1)
