7df11583a9ca10eb5d81cb3db1a28070f8ad1e7a
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-07-23 14:54:54 -0500

    Update drive_multithreading.py

--- software/python/basics/drive_multithreading.py
+++ software/python/basics/drive_multithreading.py
@@ -7,91 +7,47 @@ import time
 import numpy as np # for handling matrices
 import text2speech_ex2 as t2s # for speaking by aux port
 import threading # only used for threading functions
-# # IMPORT INTERNAL ITEMS
-import speed_control2 as sc # closed loop control. Import speed_control for open-loop
-import inverse_kinematics as inv
-import kinematics as kin
+# IMPORT INTERNAL ITEMS
+import speed_control as sc # closed loop control. Import speed_control for open-loop
+import inverse_kinematics as inv #calculates wheel parameters from chassis
+import kinematics as kin    # calculates chassis parameters from wheels
+import log # log live data to local files
+import gamepad_ex1 as gp
 # import repel as repel
 
-axes = np.zeros(16) #number of elements returned by gamepad
-
-
-def writeFiles(current_phis):
-    txt = open("/home/debian/SCUTTLE/phi_dot_data_L.txt", 'w+')
-    txt2 = open("/home/debian/SCUTTLE/phi_dot_data_R.txt", 'w+')
-    phi_dotL = round(current_phis[0],1)
-    phi_dotR = round(current_phis[1],1)
-    #print("Left Phi:", phi_dotL, "Right Phi:", phi_dotR)
-    txt.write(str(phi_dotL))
-    txt2.write(str(phi_dotR))
-    txt.close()
-    txt2.close()
+# testing proper global var initialization within inverse kin
+#axes = np.zeros(16) #number of elements returned by gamepad
 
 def loop_speak( ID ):
     while(1):
-        # myStringA = "I am scuttle robot"
-        # myStringX = "the future of meka tronics"
-        # myStringB = "Special delivery"
-        # myStringY = "gig em aggies"
-        # signals = gp.getGP()
-        # if signals[6]==1: # A button is pressed
-        #     t2s.say(myStringA)
-        # if signals[7]==1: # x button is pressed
-        #     t2s.say(myStringX)
-        # if signals[5]==1: # x button is pressed
-        #     t2s.say(myStringB)
-        # if signals[4]==1: #
-        #     t2s.say(myStringY)
+        myStringA = "I am scuttle robot"
+        myStringX = "the future of meka tronics"
+        myStringB = "Special delivery"
+        myStringY = "gig em aggies"
+        signals = gp.getGP()
+        print(signals)
+        if signals[6]==1: # A button is pressed
+            print("you pressed A.")
+            t2s.say(myStringA)
+        if signals[7]==1: # x button is pressed
+            t2s.say(myStringX)
+        if signals[5]==1: # x button is pressed
+            t2s.say(myStringB)
+        if signals[4]==1: #
+            t2s.say(myStringY)
         time.sleep(0.05)
 
 def loop_drive( ID ):
-
     while(1):
-#####################################################################
-# THIS CODE IS FOR CLOSED LOOP CONTROL
-        # #get the latest target phi_dots from inverse kinematics
-        # phis = inv.get_phis() # populates target phi dots, targets
-        # #print("phi dot left and right:", phis)
-        # kin.getPhiDots()
-        # print(current_phis)
-        # writeFiles(current_phis)
-        # #print("latest speeds:", current_phis)
-        #
-        # #call the speed control system to action:
-        # sc.driveClosdedLoop(phis[0], current_phis[0])  # Currently just working with Left
-        #                               # THis is sending left target and current
-        #
-        # # print(f"target phis: {phis}  ,  current phi dots:{current_phis} ")
-        # #print("target phis:", phis, "current phi dots:", current_phis)
-        # time.sleep(0.1)
-#######################################################################
-
-
-
-#########################################################################
-# THIS CODE IS FOR OPEN LOOP control
-            #get the latest target phi_dots from inverse kinematics
-            phis = inv.get_phis() # populates target phi dots, targets
-            #print("phi dot left and right:", phis)
-            kin.getPhiDots()
-            print("This is kin.latest_speeds: ", kin.latest_speeds)
-
-            #call the speed control system to action:
-            sc.driveOpenLoop(phis[0],phis[1])
-            #writeFiles(current_phis)
-            #print("latest speeds:", current_phis)
-
-            # print(f"target phis: {phis}  ,  current phi dots:{current_phis} ")
-            #print("target phis:", phis, "current phi dots:", current_phis)
-            time.sleep(0.1)
-################################################################################
-
-
-
-
-
-             # populate thetaDot & xDot
-            #print("thetadot, xdot", motion)
+        # THIS CODE IS FOR OPEN AND CLOSED LOOP control
+        pdTargets = inv.getPdTargets() # populates target phi dots
+        kin.getPdCurrent() # measure latest phi dots & update global variable
+        pdCurrent = kin.pdCurrent # assign the global variable value to a local var
+        sc.driveOpenLoop(pdTargets[0],pdTargets[1]) #call the speed control system to action:
+        #sc.driveClosdedLoop(pdTargets[0], pdCurrent[0])  # testing driveCL with left wheel
+        log.writeFiles(pdCurrent)
+        print("latest speeds:", pdCurrent)
+        time.sleep(0.1)
 
 def loop_scan( ID ):
     while(1):
@@ -104,14 +60,17 @@ def loop_scan( ID ):
 def main():
         print("starting the main fcn")
         threads = []  # create an object for threads
+
         t = threading.Thread( target=loop_speak, args=(1,) ) # make 1st thread object
         threads.append(t)
         t.start()
         print("started thread1")
+
         t2 = threading.Thread( target=loop_drive, args=(2,) ) # make 2nd thread object
         threads.append(t2)
         t2.start()
         print("started thread2")
+
         t3 = threading.Thread( target=loop_scan, args=(3,) ) # make 2nd thread object
         threads.append(t3)
         t3.start()
