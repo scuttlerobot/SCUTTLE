7c294571abd8261cdd05e60c7e40c225d462ce66
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-08-02 17:31:19 -0500

    Update speed_control.py

--- software/python/basics/speed_control.py
+++ software/python/basics/speed_control.py
@@ -12,6 +12,7 @@ import log
 #----------------------------------------------------------------------------
 # generate_duty is a placeholder function for the inverse kinematics program
 u_integral = 0
+
 def generate_duty(x_dot,theta_dot):
     # Calculate Left and Right Wheel Duty Cycles
     duty_r = ((  1 * (theta_dot)) + x_dot )
@@ -33,82 +34,48 @@ def openLoop(pdl,pdr):
     DRS = 0.8   # create a variable for direct-re-scaling
     duty_l = pdl * 1/9.75 * DRS  # 1 is max voltage and 9.75 is max rad/s possible
     duty_r = pdr * 1/9.75 * DRS  #
-    duties = np.array([ duty_l, duty_r ])   # put the values into an array
+    duties = np.array([ duty_l, duty_r ])     # put the values into an array
     duties[0] = sorted([-1, duties[0], 1])[1] # place bounds on duty cycle
     duties[1] = sorted([-1, duties[1], 1])[1] # place bounds on duty cycle
     return duties
 
-def driveOpenLoop(dutyl,dutyr,pdc):
+def driveOpenLoop(dutyl,dutyr):
     duties = openLoop(dutyl,dutyr)
-    log.duty_speed(duties[0], pdc[0])
-    print("Left:", duties[0],"\t Right:", duties[1])
     m.MotorL(duties[0])
     m.MotorR(duties[1])
 
-def Deadzone(dd):
-    dd = (dd * 3)
-    return dd
 
-def Nonedeadzone(dn):
-    dn = ((dn * 0.7778) + 0.2222)
-    return dn
-
-
-def driveClosedLoop(pdt, pdc):
-    #print(pdt[0],'  ',pdc[0])
-    #calculate the error
-    global u_integral
-
-    e = (pdt - pdc)
-
-    #print("EL value",eL,'  ',"Target Speed L:",pdt[0],'  ',"Current Speed L:",pdc[0],'--------'"ER value",eR,'  ',"Target Speed L:",pdt[1],'  ',"Current Speed L:",pdc[1])
-
-    # e_max = 9.7 #r/s        #define e_max
-    # u_max = 1.0 #duty       #define u_max
-    # kp = np.round(u_max/e_max, decimals=3) #calculate the kp value  #This gives you a Kp of 2.5
-    #kp = 1.0/9.7
-    kp = 0.07 #variable Kp
-    #original was kp = 0.06 and ki = 0.0012
-    ki = 0.011
-    #kd = 0.08
-    #multiply the k by the error to get U_poroportional
-    u_proportional = (e * kp)    #will re-define later, u is just the control signal
-    u_integral    += (e * ki)
-    #u_derivative += (e / dt)
-
-    #u = u_proportional
-    u = np.round((u_proportional + u_integral),3) # round to ensure driver handling
-
-    # u = u_proportional + (kd * u_derivative)
-    # u = u_proportional + (ki * u_integral) + (kd * u_derivative)
-
-    dutyL = u[0]  #sort u_proportional equal to duty
-    dutyR = u[1]
-    dutyL = sorted([-1,dutyL,1])[1]
-    dutyR = sorted([-1,dutyR,1])[1]
-
-    m.MotorL(round(dutyL,2))
-    m.MotorR(round(dutyR,2))
-    #print("left:", dutyL, "\t right:", dutyR)
-    # if (dutyL < 0.2):
-    #     pwmL = Deadzone(dutyL)
-    #     m.MotorL(pwmL)
-    #     print(pwmL)
-    #     #print(pwmL)
-    # if (dutyR < 0.2):
-    #     pwmR = Deadzone(dutyR)
-    #     m.MotorR(pwmR)
-    #     #print(pwmR)
-    # if (dutyL > 0.2):
-    #     pwmL = Nonedeadzone(dutyL)
-    #     m.MotorL(pwmL)
-    #     print(pwmL)
-    # if (dutyR > 0.2):
-    #     pwmR = Nonedeadzone(dutyR)
-    #     m.MotorR(pwmR)
-    #     #print(pwmR)
-    log.controlsignal(u_proportional, u_integral)
-    #log.error(pdt,pdc)
-
-
+def scalingFunction(x):
+    if -0.222 < x and x < 0.222:
+        y = (x * 3)
+    elif x > 0.222:
+        y = ((x * 0.778) + 0.222)
+    else:
+        y = ((x * 0.778) - 0.222)
+    return y
     
+def scaleMotorEffort(u):
+    u_out = np.zeros(2)
+    u_out[0] = scalingFunction(u[0])
+    u_out[1] = scalingFunction(u[1])
+    return(u_out)
+
+def driveClosedLoop(pdt, pdc, de_dt):
+    global u_integral
+    e = (pdt - pdc) # compute error
+    kp = 0.07  # proportional constant
+    ki = 0.011 # integral constant
+    kd = 0     # derivative constant
+    
+    # generate components of the control effort, u
+    u_proportional = (e * kp)     # proportional term
+    u_integral    += (e * ki)     # integral term 
+    u_derivative   = (de_dt * kd) # derivative term
+    # condition the signal before sending to motors 
+    u = np.round((u_proportional + u_integral),2) # must round to ensure driver handling
+    u = scaleMotorEffort(u)
+    u[0] = sorted([-1,u[0],1])[1]  # place bounds on the motor commands
+    u[1] = sorted([-1,u[1],1])[1]  # within [-1, 1]
+    # send the signal to motors
+    m.MotorL(round(u[0],2)) # must round to ensure driver handling!
+    m.MotorR(round(u[1],2)) # must round to ensure driver handling!
