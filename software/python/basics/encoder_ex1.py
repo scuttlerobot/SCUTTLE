168f310f71a88ee987b1501087252c7dcdae35ae
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-08-03 14:05:32 -0500

    Update encoder_ex1.py

--- software/python/basics/encoder_ex1.py
+++ software/python/basics/encoder_ex1.py
@@ -17,17 +17,17 @@ def readEncs(channel):
         elif channel == "R":
             x = encR.readU16(0xFE) # capture the 8 msb's from encoder
             y = encR.readU16(0xFF) # capture the 6 lsb's from encoder
-        # y can contribute 1.4 degrees to the reading at most
+        # y can contribute  at most 1.4 degrees to the reading
         # for x, perform bitwise operation to get true scaling of these bits
         x = ((x << 8) | (x >> 8)) & 0xFFFF
         x = ((x & 0xFF00) >> 2) | ( x & 0x3F)
         angle_raw = x + y  # multiplying by 0.0219 will give degrees
     except:
         print('Warning (I2C): Could not read encoder' + channel)
-        angle_raw = 0
-    return angle_raw
+        angle_raw = 0  # set to zero, avoid sending wrong value
+    return angle_raw   # the returned value must be scaled by 359deg / 2^14
 
-#the second function returns the encoder values that will be used in the rest of the programs
+# the READ function returns the encoder values.  Call this fcn from other programs.
 def read():
     encLeft = readEncs('L')
     encRight = readEncs('R')
