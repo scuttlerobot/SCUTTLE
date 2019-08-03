1baa4834bc65e6011a8c507bdecbae0dc4e02b8b
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-08-03 14:41:47 -0500

    Update lidar_ex1.py

--- software/python/basics/lidar_ex1.py
+++ software/python/basics/lidar_ex1.py
@@ -2,20 +2,14 @@
 # 1) grab a subset of the readings from the lidar for lightweight purposes
 # 2) assign the proper angle value to the reading, with respect to robot x-axis
 # 3) create a 2d array of [distances, angles] from the data
-# 4) find the point in the array that is closest to P1 (point of interest regarding collision)
 
 import numpy as np # for array handling
 import pysicktim as lidar # required for communication with TiM561 lidar sensor
 
 np.set_printoptions(suppress=True)  # Suppress Scientific Notation
-
-p0 = np.asarray([0,0]) #define p0, which is the location of the lidar (x and y).
-p1 = np.asarray([0.300,0.0]) # define p1, which is the location of interest for collisions (meters)
-
 start_angle = -135.0 # lidar points will range from -135 to 135 degrees
-num_points = 54 # Desired number of points
 
-def nearest_point():
+def polarScan(num_points):
 
     lidar.scan() #take reading
 
@@ -42,37 +36,4 @@ def nearest_point():
     scan_points = np.hstack((scan_points,angles))    # Turn two (54,) arrays into a single (54,2) matrix
     scan_points = np.round(scan_points,3) # Round each element in array to 3 decimal places
 
-    # convert the polar coordinates into cartesian
-    scan_cart = np.zeros((num_points,2))
-    d = len(scan_cart) # return the number of elements in scan
-    for d in range(d):
-        scan_cart[d,0]=scan_points[d,0]*np.cos(np.radians(scan_points[d,1]))
-        scan_cart[d,1]=scan_points[d,0]*np.sin(np.radians(scan_points[d,1]))
-
-    # calculate the distances between p0 and scan x_points
-    vector_lengths = np.zeros(num_points)
-    k = 0.004 # minimum distance for a reading considered to be valid
-    d = len(scan_cart) #return the number of elements in scan
-    for d in range(d):
-        a = scan_cart[d,0]-p1[0] # difference in x values
-        b = scan_cart[d,1]-p1[1] # difference in y values
-        c = np.sqrt(a**2 + b**2) # take the hypotenuse
-        if c < k:
-            c = 5.0 # for nonvalid measurements, raise to 5 meters
-        vector_lengths[d]= c # assign hypotenuse to vector length
-
-
-    #"filter" returns array of all values greater than k, and min finds the minimum
-    d_min = min(filter(lambda x: x > k, vector_lengths))
-    myIndex = np.argmin(vector_lengths)
-    myYValue =  scan_cart[myIndex,1] #column 1 is y values in scan_cart
-    myPoint = np.array([ d_min, myYValue ])
-    myPoint = np.round(myPoint, decimals = 3)
-    #print(d_min) #print out the closest detected obstacle
-    return(myPoint)
-
-# Current output is a (54,2) matrix
-# [distance(scan_points), theta(degrees)]
-
-# print(scan_points[:,1])
-# print(degrees)
+    return(scan_points)
