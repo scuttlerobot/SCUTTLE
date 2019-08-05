f5de00004262e5e86d7fb5568206114637b768c1
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-08-05 16:09:17 -0500

    Update L1_adc.py

--- software/python/onboard_sensors/L1_adc.py
+++ software/python/onboard_sensors/L1_adc.py
@@ -6,7 +6,6 @@ import numpy as np
 import rcpy
 from rcpy._adc import *
 
-
 def getAdc():
     A0 = round(get_voltage(0),3)    # ADC channel 0 
     A1 = round(get_voltage(1),3)    # ADC channel 1 
@@ -16,10 +15,12 @@ def getAdc():
     A5 = round(get_voltage(5),3)    # DC Input
     A6 = round(get_voltage(6),3)    # Lipo battery input
     adcData = np.array([A0, A1, A2, A3, A4, A5, A6])
-    print("\nA0:",A0, "\nA1:",A1, "\nA2:", A2, "\nA3:", A3, "\nA4:", A4, "\nA5:", A5, "\nA6:", A6)
+    # nicely formatted print statement below
+    #print("\nA0:",A0, "\nA1:",A1, "\nA2:", A2, "\nA3:", A3, "\nA4:", A4, "\nA5:", A5, "\nA6:", A6)
     return(adcData)
-    
-while 1:
-    myAdcData = getAdc()
-    print (myAdcData)
-    time.sleep(0.1)
+
+# UNCOMMENT THE SECTION BELOW TO RUN AS STANDALONE CODE    
+# while 1:
+#     myAdcData = getAdc()
+#     print (myAdcData)
+#     time.sleep(0.1)
