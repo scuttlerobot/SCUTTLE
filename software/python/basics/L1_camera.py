19d3a2baf8e05be4bff31bd1b28c55de681d49ed
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-08-07 10:20:39 -0500

    Update L1_camera.py

--- software/python/basics/L1_camera.py
+++ software/python/basics/L1_camera.py
@@ -1,5 +1,5 @@
-# this file retrieves an image from the usb camera.
-
+# This file retrieves an image from the usb camera.
+import numpy as np
 import cv2
 
 width  = 240
@@ -12,3 +12,8 @@ def newImage(size=(width, height)):
         return None
     image = cv2.resize(image,size) # resize the image
     return image
+    
+# UNCOMMENT THE SECTION BELOW TO RUN AS A STANDALONE PROGRAM
+while 1:
+    image = newImage()
+    print(image.shape) #print info about the image (height, width, colorspace)
