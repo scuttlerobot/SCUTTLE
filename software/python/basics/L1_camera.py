5735835ccda3e66900503d0c77c320a27751c5d7
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-08-07 09:51:23 -0500

    Update L1_camera.py

--- software/python/basics/L1_camera.py
+++ software/python/basics/L1_camera.py
@@ -4,15 +4,11 @@ import cv2
 
 width  = 240
 height = 160
+camera = cv2.VideoCapture(0) # This will take images only from the camera assigned as "0"
 
-camera = cv2.VideoCapture(0)
-
-def takepic(size=(width, height)):
-
+def newImage(size=(width, height)):
     ret, image = camera.read()
     if not ret:
         return None
-
     image = cv2.resize(image,size) # resize the image
-
     return image
