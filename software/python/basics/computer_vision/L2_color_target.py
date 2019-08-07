6186375bfdc2e34009c7306d24032a324e12293a
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-08-07 09:50:01 -0500

    Update L2_color_target.py

--- software/python/basics/computer_vision/L2_color_target.py
+++ software/python/basics/computer_vision/L2_color_target.py
@@ -5,8 +5,6 @@ import L1_camera as cam
 def colorTracking(img, color_range=((0,0,0),(255,255,255))):
 
     global image
-
-<<<<<<< HEAD
     image = img
 
     if filter == 'RGB':
@@ -21,53 +19,24 @@ def colorTracking(img, color_range=((0,0,0),(255,255,255))):
     # apply a blur function
     kernel = np.ones((5,5),np.uint8)
     mask = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel) # Apply blur
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel) # Blur again
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel) # Apply blur 2nd iteration
 
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2] #generates number of contiguous "1" pixels
     center = None # create a variable for x, y location of target
 
     if len(cnts) > 0:   # begin processing if there are "1" pixels discovered
-
         c = max(cnts, key=cv2.contourArea)  # return the largest target area
         ((x, y), radius) = cv2.minEnclosingCircle(c)
-        # # M = cv2.moments(c)
-        # # center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))  # defines a circle around the largest target area
-        # center = (int(x), int(y))  # defines a circle around the largest target area
-
         return ((round(x,1),round(y,1)),round(radius,1))
 
     else:
-
         return((None, None), 0)
 
-####################################################
-
+# Uncomment the section below to run as a standalone program
+#-----------------------------------------------------------
 color_range = ((0,180,130),(10,255,255))
-
-# width  = 240  # please attempt to put back into the function
-# height = 160
-
 while True:
 
-    # image = cam.takepic((width,height))
-    image = cam.takepic()
-
-    # image = cv2.resize(image,(width,height)) # resize the image
-
+    image = cam.newImage()
     ((x, y), radius) = colorTracking(image, color_range)
-
     print(x,y, "\t", radius)
-=======
-        return ((x,y),radius)
-
-    def build_image(self, img):
-
-        return all
-
-def init_filter():
-    f = MyFilter()
-    return f.process
-
-while 1:
-    color_range = ((0,180,130),(10,255,255))
->>>>>>> 6172cd6ddd4eff90e903bcb2c470fa0b5a061f43
