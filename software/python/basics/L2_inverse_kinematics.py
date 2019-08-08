7b558a01005233f8fecbd6269f7822bbdd775461
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-08-08 10:28:13 -0500

    Update L2_inverse_kinematics.py

--- software/python/basics/L2_inverse_kinematics.py
+++ software/python/basics/L2_inverse_kinematics.py
@@ -8,7 +8,7 @@ import L1_gamepad as gp # to call getGP from gamepad
 # define robot geometry
 R = 0.041 # wheel radius
 L = 0.201 # half of the wheelbase
-A = np.array([[1/R, L/R],[1/R, -L/R]]) # matrix A * [xd, td] = [pdl, pdr] 
+A = np.array([[1/R, -L/R],[1/R, L/R]]) # matrix A * [xd, td] = [pdl, pdr] 
 
 # define constraints for theta and x speeds
 max_xd = 0.4 # maximum achievable x_dot (m/s) FW  translation
@@ -21,8 +21,8 @@ def map_speeds(B): # this function will map the gamepad speeds to max values
     return(B_mapped)
 
 def populate_gp():
-    gpData = gp.getGP() #when there is no controller input, update is empty
-    global axes
+    gpData = gp.getGP() # when there is no controller input, update is empty
+    global axes         # create a global variable for other programs to access
     try:                      # when update has no data, update.size DNE
         if gpData.size == 16:  # if update has data, store it to axes
             axes = gpData # now axes is a 16-element array
@@ -31,13 +31,13 @@ def populate_gp():
 
     # assign axes from gamepad to requested velocities
     x_dot = -1*axes[1] # assign forward axis, inverted
-    theta_dot = -1*axes[0] # assign L/R axis, non inverted
-    print("TD:", theta_dot,"XD:",x_dot)
-    B_raw = np.array([theta_dot, x_dot]) # form the B matrix
+    theta_dot = -1*axes[0] # assign L/R axis, inverted
+    B_raw = np.array([x_dot, theta_dot]) # form the B matrix
     B = map_speeds(B_raw) # re map the values to within max achievable speeds
+    print("td:", B[1])
     return(B)
 
-# convert will take the "B" matrix containing [x_dot, theta_dot]
+# Convert will take the "B" matrix containing [x_dot, theta_dot]
 # and return the C matrix containing [phi_dot_L, phi_dot_R]
 def convert(B):
     C = np.matmul(A, B)  # matrix multiplication
@@ -48,7 +48,6 @@ def getPdTargets():
     B = populate_gp() # retrieves targets in [xdot, thetadot] form
     C = convert(B) # convert the targets to [pdl, pdr] form
     return(C)
-
 
 #create a function that can convert an obstacle into an influence on theta dot
 def phi_influence(yValue):
@@ -68,6 +67,6 @@ def phi_influence(yValue):
 #     x_dot = 1 #meters per second
 #     theta_dot = 0 # radians per second
 #     B = np.array([x_dot, theta_dot])
-#     phis = convert(B)
-#     print(phis[0])
+#     phis = convert(B) # convert [xd, td] to [pdl, pdr]
+#     print(phis[0]) # print pdl
 #     time.sleep(0.5)
