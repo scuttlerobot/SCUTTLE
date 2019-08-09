115f2826153474200f0c84e2a93b20c8b3513cfc
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-08-09 16:24:49 -0500

    Update L3_drive_mt.py

--- software/python/basics/L3_drive_mt.py
+++ software/python/basics/L3_drive_mt.py
@@ -1,42 +1,56 @@
-# This program demonstrates importing of other python files and
-# calling functions from child files.
-# last updated 2019.05.23
+# Level 3 program for driving SCUTTLE and handling other tasks in parallel
 
 # IMPORT EXTERNAL ITEMS
 import time
 import numpy as np # for handling matrices
 import threading # only used for threading functions
 import math
+
 # IMPORT INTERNAL ITEMS
 import L2_speed_control as sc # closed loop control. Import speed_control for open-loop
 import L2_inverse_kinematics as inv #calculates wheel parameters from chassis
 import L2_kinematics as kin    # calculates chassis parameters from wheels
 import L2_log # log live data to local files
+import L1_text2speech as t2s # for speaking over aux port
 import L1_gamepad as gp
 import L1_encoder as enc
-import L1_text2speech as t2s # for speaking by aux port
+import L1_bmp as bmp
+import L1_adc as adc
+import L1_rssi as rssi
 # import L2_obstacle as obs
+# import L2_color_target as ct
 
 def loop_speak( ID ):
+    myStringA = "I am scuttle robot"
+    myStringX = "the future of meka tronics"
+    myStringY = "gig em aggies"
     while(1):
-        myStringA = "I am scuttle robot"
-        myStringX = "the future of meka tronics"
-        myStringB = "Special delivery"
-        myStringY = "gig em aggies"
         signals = gp.getGP()
         if signals[6]==1: # A button is pressed
-            print("you pressed A.")
             t2s.say(myStringA)
-        if signals[7]==1: # x button is pressed
+        elif signals[7]==1: # X button is pressed
             t2s.say(myStringX)
-        if signals[5]==1: # x button is pressed
+        elif signals[5]==1: # B button is pressed
+            temp = round(bmp.getTemp(),1)
+            myStringB = "My brain temperature is" +str(temp)+ " degrees celsius."
             t2s.say(myStringB)
-        if signals[4]==1: #
+        elif signals[4]==1: # Y button is pressed
             t2s.say(myStringY)
-        time.sleep(0.05)
+        elif signals[11]==1: # rt button is pressed (button 7)
+            adcData = adc.getADC()
+            voltage = round(adcData[5],1)
+            myString = "My battery strength is" +str(voltage)+ " volts."
+            t2s.say(myString)
+        elif signals[10]==1: 
+            #myRSSI = rssi.get_rssi('wlan0')
+            myRSSI = -71.4
+            myString = "wifi rssi is" +str(myRSSI)+ " decibels."
+            t2s.say(myString)
+        time.sleep(0.020)
 
 def loop_drive( ID ):
     
+    # initialize variables for control system
     t0 = 0
     t1 = 1
     e00 = 0
@@ -44,40 +58,64 @@ def loop_drive( ID ):
     e1 = 0
     dt = 0
     de_dt = np.zeros(2) # initialize the de_dt
+    # initialize variables for color tracking
+    color_range = np.array([[0, 180, 130], [10, 255, 255]])
+    thetaOffset = 0
+    # initialize driving mode
+    mode = 0
     
     while(1):
-
+        
+        # THIS BLOCK IS FOR COLOR TRACKING
+        # colorTarget = ct.colorTarget(color_range) # use color tracking to generate targets
+        # x = colorTarget[0]
+        # if x != None:
+        #     thetaOffset = ct.horizLoc(x) #grabs the angle of the target in degrees
+        # myThetaDot = thetaOffset * 3.14/180 *2 # achieve centering in 0.5 seconds
+        # print(myThetaDot)
+        # myXDot = 0
+        # A = np.array([ myXDot, myThetaDot ])
+        # pdTargets = inv.convert(A) # convert [xd, td] to [pdl, pdr]
+        # print(pdTargets)
+        
+        # THIS BLOCK IS FOR CONTINUOUS DRIVING
+        # constantThetaDot = 1.5 # target, rad/s
+        # constantXDot = 0.2 # target, m/sc
+        # A = np.array([constantXDot, constantThetaDot])
+        # myPhiDots = inv.convert(A)
+       
         # THIS CODE IS FOR OPEN AND CLOSED LOOP control
         pdTargets = inv.getPdTargets() # populates target phi dots from GamePad
         kin.getPdCurrent() # capture latest phi dots & update global var
         pdCurrents = kin.pdCurrents # assign the global variable value to a local var
-
-        # The following block is for continuous driving
-        # myThetaDot = 1.5 # target, rad/s
-        # myXDot = 0.2 # target, m/sc
-        # A = np.array([myXDot, myThetaDot])
-        # myPhiDots = inv.convert(A)
-        
+       
+        # THIS BLOCK UPDATES VARS FOR CONTROL SYSTEM
         t0 = t1  # assign t0
         t1 = time.time() # generate current time
         dt = t1 - t0 # calculate dt
-        #print("dt = ", round(dt,3))
         e00 = e0 # assign previous previous error
         e0 = e1  # assign previous error
         e1 = pdCurrents - pdTargets # calculate the latest error
         de_dt = (e1 - e0) / dt # calculate derivative of error
-        sc.driveClosedLoop(pdTargets, pdCurrents, de_dt)  # call the control system
-        if (pdCurrents[0] > pdTargets[0]):
-            print(count)
-            break
-    log.csv_write([count, pdCurrents[0], pdCurrents[1]])
+        
+        # CALLS THE CONTROL SYSTEM TO ACTION
+        signals = gp.getGP() # grab gamepad input
+        if signals[14]: # check if mode button was pressed (Left stick)
+            mode = not mode 
+            time.sleep(1)
+            #t2s.say("changing modes.")
+        if mode == 0:
+            sc.driveOpenLoop(pdTargets)
+        else:
+            sc.driveClosedLoop(pdTargets, pdCurrents, de_dt)  # call the control system
+        time.sleep(0.05)
 
 def loop_scan( ID ):
     while(1):
-            # d = obs.nearest_point() #returns the distance and the y-value of nearest obstacle
-            # #print("nearest point:", d)
-            # if d[0] < 0.15: #if the nearest obstacle less than 10cm
-            #         t2s.say("NO")
+        #d = obs.nearest_point() #returns the distance and the y-value of nearest obstacle
+        #print("nearest point:", d)
+        # if d[0] < 0.15: #if the nearest obstacle less than 10cm
+        #         t2s.say("NO")
         time.sleep(0.1)
 
 def main():
