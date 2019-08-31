ebd17cbdef81181ac0a9945ee6acd627136bf4c8
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-08-31 10:37:03 -0500

    Update and rename software/python/basics/onboard_sensors/L1_mpu.py to software/python/basics/L1_mpu.py

--- software/python/basics/L1_mpu.py
+++ software/python/basics/L1_mpu.py
@@ -1,32 +1,27 @@
-# This program reads accelerometer, gyro, and magnetometer data from the onboard MPU9250 sensor.
+# This program accesses info from the Blue's onboard sensor, MPU9250
+# It reads accelerometer, gyro, and magnetometer data from the sensor.
 
 #!/usr/bin/env python3
 # import python libraries
 import time
 import getopt, sys
 
-# import rcpy library
-# This automatically initizalizes the robotics cape
+# import rcpy library (this automatically initializes robotics cape)
 import rcpy
 import rcpy.mpu9250 as mpu9250
 
 # set state to rcpy.RUNNING
 rcpy.set_state(rcpy.RUNNING)
 
-# no magnetometer
-# mpu9250.initialize(enable_magnetometer = True)
+# mpu9250.initialize(enable_magnetometer = True) # by default, mag is not initialized. Uncomment for mag usage.
 mpu9250.initialize()
 
-# keep running
+# UNCOMMENT THE SECTION BELOW TO RUN AS A STANDALONE PROGRAM
 while True:
-
-    # running
-    if rcpy.get_state() == rcpy.RUNNING:
-
-        # temp = mpu9250.read_imu_temp()
+    if rcpy.get_state() == rcpy.RUNNING: # verify the rcpy package is running
+        # temp = mpu9250.read_imu_temp() # this command returns just temperature
         data = mpu9250.read() # this command returns a string with many parameters.
-
         print("Accel,\tX:{} m/s^2\t Y:{} m/s^2\t Z:{} m/s^2\t\tGyro, X:{} deg/s\t Y:{} deg/s\t Z:{} deg/s".format(round(data['accel'][0],3) , round(data['accel'][1],3) , round(data['accel'][2],3) , round(data['gyro'][0],3) , round(data['gyro'][1],3) , round(data['gyro'][2],3) ))
 
     # sleep some
-    time.sleep(.5)
+    time.sleep(.5) # delay
