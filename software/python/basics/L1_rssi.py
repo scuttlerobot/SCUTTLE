da19b1197dba7fb3a2883a2cea3d8b487c3b4006
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-09-02 09:46:59 -0500

    Update L1_rssi.py

--- software/python/basics/L1_rssi.py
+++ software/python/basics/L1_rssi.py
@@ -6,6 +6,7 @@
 
 import sys
 import subprocess
+import time
 
 def get_rssi(interface): # ie, 'wlan0'
     proc = subprocess.Popen(["iwlist", interface, "scan"],stdout=subprocess.PIPE, universal_newlines=True)
