b605505176fa0d11c85da1f099b34d464e40d5b7
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-09-04 15:39:25 -0500

    Update L1_mpu.py

--- software/python/basics/L1_mpu.py
+++ software/python/basics/L1_mpu.py
@@ -17,11 +17,11 @@ rcpy.set_state(rcpy.RUNNING)
 mpu9250.initialize()
 
 # UNCOMMENT THE SECTION BELOW TO RUN AS A STANDALONE PROGRAM
-while True:
-    if rcpy.get_state() == rcpy.RUNNING: # verify the rcpy package is running
-        # temp = mpu9250.read_imu_temp() # this command returns just temperature
-        data = mpu9250.read() # this command returns a string with many parameters.
-        print("Accel,\tX:{} m/s^2\t Y:{} m/s^2\t Z:{} m/s^2\t\tGyro, X:{} deg/s\t Y:{} deg/s\t Z:{} deg/s".format(round(data['accel'][0],3) , round(data['accel'][1],3) , round(data['accel'][2],3) , round(data['gyro'][0],3) , round(data['gyro'][1],3) , round(data['gyro'][2],3) ))
+# while True:
+#     if rcpy.get_state() == rcpy.RUNNING: # verify the rcpy package is running
+#         # temp = mpu9250.read_imu_temp() # this command returns just temperature
+#         data = mpu9250.read() # this command returns a string with many parameters.
+#         print("Accel,\tX:{} m/s^2\t Y:{} m/s^2\t Z:{} m/s^2\t\tGyro, X:{} deg/s\t Y:{} deg/s\t Z:{} deg/s".format(round(data['accel'][0],3) , round(data['accel'][1],3) , round(data['accel'][2],3) , round(data['gyro'][0],3) , round(data['gyro'][1],3) , round(data['gyro'][2],3) ))
 
-    # sleep some
-    time.sleep(.5) # delay
+#     # sleep some
+#     time.sleep(.5) # delay
