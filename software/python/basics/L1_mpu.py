431cb4111d8bae10ca707fdcf409170689eacf86
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-09-04 15:40:02 -0500

    Update L1_mpu.py

--- software/python/basics/L1_mpu.py
+++ software/python/basics/L1_mpu.py
@@ -2,26 +2,39 @@
 # It reads accelerometer, gyro, and magnetometer data from the sensor.
 
 #!/usr/bin/env python3
-# import python libraries
-import time
+import time # for time.sleep function
 import getopt, sys
+import numpy as np
 
-# import rcpy library (this automatically initializes robotics cape)
-import rcpy
+import rcpy # import rcpy library (this automatically initializes robotics cape)
 import rcpy.mpu9250 as mpu9250
 
-# set state to rcpy.RUNNING
-rcpy.set_state(rcpy.RUNNING)
 
-# mpu9250.initialize(enable_magnetometer = True) # by default, mag is not initialized. Uncomment for mag usage.
-mpu9250.initialize()
+rcpy.set_state(rcpy.RUNNING) # set state to rcpy.RUNNING
+mpu9250.initialize(enable_magnetometer = True) # by default, mag is not initialized. Uncomment for mag usage.
+mpu9250.initialize() # initialize the sensor
+
+def getAccel():
+    data = mpu9250.read() # this command returns a string with many parameters.
+    a1 = round(data['accel'][0],3)
+    a2 = round(data['accel'][1],3)
+    a3 = round(data['accel'][2],3)
+    axes = np.array([a1, a2, a3])
+    return(axes)
+    
+def getAll():
+    data = mpu9250.read() # this command returns a string with many parameters.
+    return(data)
+    
+def getTemp():
+    temp = mpu9250.read_imu_temp() # this command returns just temperature
 
 # UNCOMMENT THE SECTION BELOW TO RUN AS A STANDALONE PROGRAM
 # while True:
 #     if rcpy.get_state() == rcpy.RUNNING: # verify the rcpy package is running
-#         # temp = mpu9250.read_imu_temp() # this command returns just temperature
-#         data = mpu9250.read() # this command returns a string with many parameters.
-#         print("Accel,\tX:{} m/s^2\t Y:{} m/s^2\t Z:{} m/s^2\t\tGyro, X:{} deg/s\t Y:{} deg/s\t Z:{} deg/s".format(round(data['accel'][0],3) , round(data['accel'][1],3) , round(data['accel'][2],3) , round(data['gyro'][0],3) , round(data['gyro'][1],3) , round(data['gyro'][2],3) ))
+#         data = getAll()
+#         print("Accel,\tX:{} m/s^2\t Y:{} m/s^2\t Z:{} m/s^2\t\tGyro, X:{} deg/s\t Y:{} deg/s\t Z:{} deg/s" \
+#         .format(round(data['accel'][0],3) , round(data['accel'][1],3) , round(data['accel'][2],3) , round(data['gyro'][0],3) , \
+#         round(data['gyro'][1],3) , round(data['gyro'][2],3) ))
 
-#     # sleep some
-#     time.sleep(.5) # delay
+#     time.sleep(.5) # delay 0.5s
