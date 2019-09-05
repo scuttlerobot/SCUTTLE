4c1e25967614f7844cf45bd90b0fcc14a6ab5048
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-09-04 21:16:03 -0500

    Update L1_adc.py

--- software/python/basics/L1_adc.py
+++ software/python/basics/L1_adc.py
@@ -13,21 +13,20 @@ def getAdc():
     A2 = round(get_voltage(2),3)    # ADC channel 2 
     A3 = round(get_voltage(3),3)    # ADC channel 3 
     A4 = round(get_voltage(4),3)    # ADC channel 4 
-    A5 = round(get_voltage(5),3)    # DC Input
-    A6 = round(get_voltage(6),3)    # Lipo battery input
+    A5 = round(get_voltage(5),3)    # DC Input (unscaled)
+    A6 = round(get_voltage(6),3)    # Lipo battery input (unscaled)
     adcData = np.array([A0, A1, A2, A3, A4, A5, A6])
     # nicely formatted print statement below
     #print("\nA0:",A0, "\nA1:",A1, "\nA2:", A2, "\nA3:", A3, "\nA4:", A4, "\nA5:", A5, "\nA6:", A6)
     return(adcData)
 
-def getDcJack():
+def getDcJack(): # return the voltage measured at the barrel plug
     voltage = round(get_dc_jack_voltage(), 2)
     return (voltage)
 
 # UNCOMMENT THE SECTION BELOW TO RUN AS STANDALONE CODE    
 while 1:
-    myAdcData = getAdc()
-    print (myAdcData)
+    adcData = getAdc()
     dcJack = getDcJack()
-    print("battery:", dcJack, "v")
+    print("adc (v):", adcData, "\t battery (v):", dcJack)
     time.sleep(0.5)
