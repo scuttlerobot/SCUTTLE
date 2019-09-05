ec974b15dfe2d7654dbe36dbefb9482ea1912e23
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-09-04 20:50:14 -0500

    Update L3_telemetry.py

--- software/python/basics/L3_telemetry.py
+++ software/python/basics/L3_telemetry.py
@@ -1,5 +1,6 @@
 # L3_telemetry.py
-# This file will log data to NodeRed.
+# This program grabs data from the onboard sensors and log data in files
+# for NodeRed access and integrate into a custom "flow".
 # Access nodered at 192.168.8.1:1880 (by default, it's running on the Blue)
 
 # Import Internal Programs
@@ -13,9 +14,9 @@ import time
 # Run the main loop    
 while 1:
     accel = mpu.getAccel() # call the function from within L1_mpu.py
-    (xAcc) = accel[0] # x axis is stored in the first element
-    (yAcc) = accel[1] # y axis is stored in the second element
-    print ("x axis:", xAcc, "y axis:", yAcc) # print the two values
-    axes = np.array([xAcc, yAcc]) #store just 2 axes
-    log.Node_Red2(axes)
+    (xAccel) = accel[0] # x axis is stored in the first element
+    (yAccel) = accel[1] # y axis is stored in the second element
+    print ("x axis:", xAccel, "y axis:", yAccel) # print the two values
+    axes = np.array([xAccel, yAccel]) #store just 2 axes in an array
+    log.NodeRed2(axes) # send the data to 
     time.sleep(0.2)
