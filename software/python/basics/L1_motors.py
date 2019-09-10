495a85396557484c853b2a7062ee36e9d07446ab
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-09-09 23:08:14 -0500

    Update L1_motors.py

--- software/python/basics/L1_motors.py
+++ software/python/basics/L1_motors.py
@@ -1,30 +1,31 @@
 # This example drives the right and left motors.
-# Intended for Beagle hardware
+# Intended for Beaglebone Blue hardware.
+# This example uses rcpy library. Documentation: guitar.ucsd.edu/rcpy/rcpy.pdf
 
 import rcpy
 import rcpy.motor as motor
 import time # only necessary if running this program as a loop
 
-motor_r = 2 	# Right Motor
 motor_l = 1 	# Left Motor
-rcpy.set_state(rcpy.RUNNING)
+motor_r = 2 	# Right Motor
 
-#channel refers to left(0) or right(1)
-def MotorL(speed):
+rcpy.set_state(rcpy.RUNNING) # initialize the rcpy library
+
+# define functions to command motors, effectively controlling PWM
+def MotorL(speed): # takes argument in range [-1,1]
     motor.set(motor_l, speed)
 
-def MotorR(speed):
+def MotorR(speed): # takes argument in range [-1,1]
     motor.set(motor_r, speed)
 
 # Uncomment this section to run this program as a standalone loop
-# while rcpy.get_state() != rcpy.EXITING:
-#
-#     if rcpy.get_state() == rcpy.RUNNING:
-#
-#         MotorL(0.5)  # gentle speed for testing program. 0.3 PWM may not spin wheels.
-#         MotorR(0.5)
-#         time.sleep(4) # run fwd for 4 seconds
-#         MotorL(-0.5)
-#         MotorR(-0.5)
-#         time.sleep(2) # run reverse for 2 seconds
-
+while rcpy.get_state() != rcpy.EXITING: # exit loop if rcpy not ready
+    if rcpy.get_state() == rcpy.RUNNING: # execute loop when rcpy is ready
+        print("L1_motors.py: driving fwd")
+        MotorL(0.5)  # gentle speed for testing program. 0.3 PWM may not spin the wheels.
+        MotorR(0.5)
+        time.sleep(4) # run fwd for 4 seconds
+        print("L1_motors.py: driving reverse")
+        MotorL(-0.5)
+        MotorR(-0.5)
+        time.sleep(2) # run reverse for 2 seconds
