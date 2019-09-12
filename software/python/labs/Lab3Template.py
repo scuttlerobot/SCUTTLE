7232b998490c77902987ff2c9847f5e2daa84f56
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-09-12 00:44:17 -0500

    Create Lab3Template.py

---
+++ software/python/labs/Lab3Template.py
@@ -0,0 +1,49 @@
+# L3_drivingPatterns.py
+# Team Number:
+# Hardware TM:
+# Software TM:
+# Date:
+# Code purpose: 
+
+# Import Internal Programs
+import L2_speed_control as sc
+import L2_inverse_kinematics as inv
+
+# Import External programs
+import numpy as np
+import time
+
+def task2()
+	myVelocities = np.array([1, 1]) #input your first pair
+	myPhiDots = inv.convert(myVelocities)
+	sc.driveOpenLoop(myPhiDots)
+	time.sleep(2) # input your duration (s)
+	
+	myVelocities = np.array([1, 1]) #input your 2nd pair
+	myPhiDots = inv.convert(myVelocities)
+	sc.driveOpenLoop(myPhiDots)
+	time.sleep(2) # input your duration (s)
+	
+	myVelocities = np.array([1, 1]) #input your 3rd pair
+	myPhiDots = inv.convert(myVelocities)
+	sc.driveOpenLoop(myPhiDots)
+	time.sleep(2) # input your duration (s)
+	
+	myVelocities = np.array([1, 1]) #input your 4th pair
+	myPhiDots = inv.convert(myVelocities)
+	sc.driveOpenLoop(myPhiDots)
+	time.sleep(2) # input your duration (s)
+	
+	myVelocities = np.array([1, 1]) #input your 5th pair
+	myPhiDots = inv.convert(myVelocities)
+	sc.driveOpenLoop(myPhiDots)
+	time.sleep(2) # input your duration (s)
+	
+	myVelocities = np.array([1, 1]) #input your 6th pair
+	myPhiDots = inv.convert(myVelocities)
+	sc.driveOpenLoop(myPhiDots)
+	time.sleep(2) # input your duration (s)
+	
+while 1:
+    task2()
+    time.sleep(0.2)
