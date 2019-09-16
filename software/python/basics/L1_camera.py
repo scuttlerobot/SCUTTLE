f1f2683ab06fce390fe69ebe1f0f0f0cd2c5b64c
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-09-16 17:23:07 -0500

    Update L1_camera.py

--- software/python/basics/L1_camera.py
+++ software/python/basics/L1_camera.py
@@ -1,16 +1,20 @@
 # This file retrieves an image from the usb camera.
+
+# Import external libraries
 import numpy as np
 import cv2
 
-width  = 240
-height = 160
+# Initialize important vars
+width  = 240 # desired width in pixels
+height = 160 # desired height in pixels
 camera = cv2.VideoCapture(0) # This will take images only from the camera assigned as "0"
 
+# A function to capture an image & return all pixel data
 def newImage(size=(width, height)):
-    ret, image = camera.read()
-    if not ret:
-        return None
-    image = cv2.resize(image,size) # resize the image
+    ret, image = camera.read() # return the image as well as ret 
+    if not ret: # (ret is a boolean for returned successfully?)
+        return None # (return an empty var if the image could not be captured)
+    image = cv2.resize(image,size) # reduce size of image
     return image
     
 # # UNCOMMENT THE SECTION BELOW TO RUN AS A STANDALONE PROGRAM
