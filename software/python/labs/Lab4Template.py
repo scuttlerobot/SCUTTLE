90113a251b89ebfa8ecb51189e91ce8452a0408b
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-09-18 22:46:04 -0500

    Create Lab4Template.py

---
+++ software/python/labs/Lab4Template.py
@@ -0,0 +1,23 @@
+# Lab4Template.py
+# Team Number:
+# Hardware TM:
+# Software TM:
+# Date:
+# Code purpose: 
+
+# Import Internal Programs
+import L2_kinematics as kin
+import L2_log as log
+
+# Import External programs
+import numpy as np
+import time
+
+# DEFINE THE FUNCTIONS FOR THE PROGRAM
+def task2()
+  #do some items here
+
+# UNCOMMENT THE LOOP BELOW TO RUN THE PROGRAM CONTINUOUSLY
+while 1:
+    task2()
+    time.sleep(0.2) # delay a short period
