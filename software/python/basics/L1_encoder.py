da29b87b6f00a6f063441a295a94d2e7f9640914
RMC-800 <50638575+RMC-800@users.noreply.github.com>
2019-09-24 22:42:23 -0500

    Update L1_encoder.py

--- software/python/basics/L1_encoder.py
+++ software/python/basics/L1_encoder.py
@@ -2,45 +2,43 @@
 # Left has address 40 and right has 41
 # Code for Beagle Hardware
 
-# Import external libraries
-import Adafruit_GPIO.I2C as Adafruit_I2C # for i2c communication functions
+import Adafruit_GPIO.I2C as Adafruit_I2C
 import time
-import numpy as np # for handling arrays
+import numpy as np
 
 encL = Adafruit_I2C.Device(0x40,1) # encoder i2c address
 encR  = Adafruit_I2C.Device(0x41,1) # encoder i2c address
-
-# the readEncs function communicates to one device in one function call
-def readEnc(channel):
+def readEncs(channel):
     try:
         # The AS5048B encoder gives a 14 bit angular reading
         if channel == 'L':
-            msB = encL.readU16(0xFE) # capture the 8 msb's from encoder
-            lsB = encL.readU16(0xFF) # capture the 6 lsb's from encoder
+            x = encL.readU16(0xFE) # capture the 8 msb's from encoder
+            y = encL.readU16(0xFF) # capture the 6 lsb's from encoder
         elif channel == "R":
-            msB = encR.readU16(0xFE) # capture the 8 msb's from encoder
-            lsB = encR.readU16(0xFF) # capture the 6 lsb's from encoder
-        # lsB can contribute  at most 1.4 degrees to the reading
-        # for msB, perform bitwise operation to get true scaling of these bits
-        msB = ((msB << 8) | (msB >> 8)) & 0xFFFF
-        msB = ((msB & 0xFF00) >> 2) | ( msB & 0x3F)
-        angle_raw = msB + lsB  # multiplying by 0.0219 will give degrees
+            x = encR.readU16(0xFE) # capture the 8 msb's from encoder
+            y = encR.readU16(0xFF) # capture the 6 lsb's from encoder
+        # y can contribute  at most 1.4 degrees to the reading
+        # for x, perform bitwise operation to get true scaling of these bits
+        x = ((x << 8) | (x >> 8)) & 0xFFFF
+        x = ((x & 0xFF00) >> 2) | ( x & 0x3F)
+        angle_raw = x   # multiplying by 0.0219 will give degrees
     except:
-        print('Warning (I2C): Could not read encoder ' + channel)
+        print('Warning (I2C): Could not read encoder' + channel)
         angle_raw = 0  # set to zero, avoid sending wrong value
-    return angle_raw   # the returned value must be scaled by ( 359deg / 2^14 )
-    
-# the READ function returns both encoder values.  Call this fcn from other programs.
+    return angle_raw   # the returned value must be scaled by 359deg / 2^14
+
+# the READ function returns the encoder values.  Call this fcn from other programs.
 def read():
-    encLeft = readEnc('L') # call for left enc value
-    encRight = readEnc('R') # call for right enc value
-    encoders = np.array([encLeft,encRight]) # form array from left and right
+    encLeft = readEncs('L')
+    encRight = readEncs('R')
+    encoders = np.array([encLeft,encRight])
     return encoders
 
 # UNCOMMENT THIS SECTION TO USE ENCODER_EX2.PY AS A STANDALONE PROGRAM
 # ------------------------------------------------------------------------------
 # while 1:
 #       encoders = read()
-#       encoders = np.round((0.0219 * encoders), 2) # scale values to get degrees
-#       print("encoders: ", encoders) # print the values
-#       time.sleep(0.10) # short delay
+#       encoders = 0.0219 * encoders
+#       # round the values and print them
+#       print("encoders: ", encoders)
+#       time.sleep(0.1)
