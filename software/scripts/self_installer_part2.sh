d9853c28753d2e81a73ccc938d76e1e20804ef94
Daniyal Ansari <daniyal.s.ansari+github@gmail.com>
2019-09-25 09:35:51 -0500

    Broken apt-get into 4 lines

--- software/scripts/self_installer_part2.sh
+++ software/scripts/self_installer_part2.sh
@@ -15,7 +15,7 @@
   python3-serial python3-opencv python3-numpy python3-opengl libsdl-ttf2.0-dev libsmpeg-dev libsdl1.2-dev libportmidi-dev libswscale-dev libavformat-dev \
   libavcodec-dev libtiff5-dev fluid-soundfont-gm timgm6mb-soundfont xfonts-base xfonts-100dpi xfonts-75dpi xfonts-cyrillic fontconfig fonts-freefont-ttf \ 
   python3-setuptools libfreetype6-dev build-essential python-smbus python3-pyaudio libsdl-image1.2-dev libsdl-mixer1.2-dev mjpg-streamer-opencv-python
-
+ 
 # Install Python Libraries
 
   sudo pip install --upgrade Adafruit_BBIO
