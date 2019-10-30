8e8ff25d2c5c5f7b4d39aaa3feae58c906525261
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-10-30 10:01:16 -0500

    Update L1_lidar.py

--- software/python/basics/L1_lidar.py
+++ software/python/basics/L1_lidar.py
@@ -1,17 +1,18 @@
-# Installation of pysicktim library is required for this program.
-
 # This File performs the following:
 # 1) grab a subset of the readings from the lidar for lightweight purposes
 # 2) assign the proper angle value to the reading, with respect to robot x-axis
 # 3) create a 2d array of [distances, angles] from the data
+# Note: installation of pysicktim library is required for to run this program.
 
+# Import external libraries
 import numpy as np # for array handling
 import pysicktim as lidar # required for communication with TiM561 lidar sensor
+import time # for timekeeping
 
 np.set_printoptions(suppress=True)  # Suppress Scientific Notation
 start_angle = -135.0 # lidar points will range from -135 to 135 degrees
 
-def polarScan(num_points):
+def polarScan(num_points=54):
 
     lidar.scan() #take reading
 
@@ -28,7 +29,7 @@ def polarScan(num_points):
     scan_points = np.reshape(scan_points,(scan_points.shape[0],1)) # Turn scan_points row into column
 
     #create the column of angles
-    angles = np.zeros(54)
+    angles = np.zeros(num_points)
     x = len(angles)
     for i in range(x): #run this loop
         angles[i] = (i*lidar.scan.dist_angle_res*lidar.scan.dist_data_amnt/num_points)+(start_angle)
@@ -39,9 +40,9 @@ def polarScan(num_points):
     scan_points = np.round(scan_points,3) # Round each element in array to 3 decimal places
 
     return(scan_points)
-
+    
 # UNCOMMENT THIS SECTION TO RUN AS A STANDALONE PROGRAM
-# while (1):
-#     lidarData = polarScan(50)
-#     print(LidarData)
-#     time.sleep(2)
+while (1):
+    lidarData = polarScan(54)
+    print(lidarData)
+    time.sleep(2)
