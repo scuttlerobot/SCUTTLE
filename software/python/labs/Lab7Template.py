d8fb2f6ad0c2c703371e6ea9acfa96f196ab32bd
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-10-31 08:25:37 -0500

    Update Lab7Template.py

--- software/python/labs/Lab7Template.py
+++ software/python/labs/Lab7Template.py
@@ -1,4 +1,24 @@
-#DRIVING BY COLOR TRACKING WITH CLOSED-LOOP CONTROL
+# Lab7Template.py
+# Team Number:
+# Hardware TM:
+# Software TM:
+# Date:
+# Code purpose: DRIVING BY COLOR TRACKING WITH CLOSED-LOOP STEERING CONTROL
+#   This program captures an image, discovers a target within your HSV range
+#   Generates a pixel location of the target, and estimates the angle of the
+#   target from the camera's centerline (x-y plane) called ThetaOffset.
+
+# Import external libraries
+import numpy as np
+import time
+
+# Import internal programs
+import L2_color_target as ct
+import L2_kinematics as kin
+import L2_speed_control as sc
+import L2_inverse_kinematics as inv
+import L2_log as log
+#import L2_joint as joint
 
 # initialize variables for control system
 t0 = 0
@@ -11,22 +31,26 @@ de_dt = np.zeros(2) # initialize the de_dt
 count = 0
 
 # initialize variables for color tracking
-color_range = np.array([[45, 123, 83], [255, 135, 170]])  
-thetaOffset = 0
+color_range = np.array([[0, 185, 100], [175, 220, 175]]) # Input your HSV choices
+thetaOffset = 0 # the measured offset of target (degrees)
+x = 0 # target center location (pixels)
 
 while(1):
-    ts0 = time.time()
-    colorTarget = ct.colorTarget(color_range) # use color tracking to generate targets
+    ts0 = time.time() # check time
+    colorTarget = ct.colorTarget(color_range) # use color tracking to generate target (x,y,radius)
     #print(colorTarget)
-    ts1 = time.time() - ts0
-    x = colorTarget[0]  # assign the x pixel location of the target
-    if x != None:
-        thetaOffset = ct.horizLoc(x) # grabs the angle of the target in degrees
+    ts1 = time.time() - ts0 # check image processing time
+    log.tmpFile(ts1, "cvSpeed.txt") 
+    if colorTarget != None:
+        x = colorTarget[0]  # assign the x pixel location of the target
+        if x != None:
+            thetaOffset = ct.horizLoc(x) # grabs the angle of the target in degrees
+    #joint.one(thetaOffset) # request pointer to point to the target.
     myThetaDot = thetaOffset * 3.14/180 *2 # attempt centering in 0.5 seconds
-    print("processing (s):", round(ts1,4), "myTD (rad/s):", round(myThetaDot,3))
+    print("x, pixels:", x, "T-offset:", thetaOffset, "processing (s):", round(ts1,4), "myTD (rad/s):", round(myThetaDot,3))
     myXDot = 0 # freeze the forward driving
     
-    # Build speed targets array & send
+    # BUILD SPEED TARGETS ARRAY
     A = np.array([ myXDot, myThetaDot ])
     pdTargets = inv.convert(A) # convert from [xd, td] to [pdl, pdr]
     kin.getPdCurrent() # capture latest phi dots & update global var
@@ -43,4 +67,5 @@ while(1):
     
     # CALLS THE CONTROL SYSTEM TO ACTION
     sc.driveClosedLoop(pdTargets, pdCurrents, de_dt)  # call the control system
+    # sc.driveOpenLoop(pdTargets)  # call the control system
     time.sleep(0.005) # very small delay.
