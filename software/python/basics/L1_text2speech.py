6e8ec0e8da8375524824b6010ae3d7c604d65fc3
RMC-800 <50638575+RMC-800@users.noreply.github.com>
2019-10-31 15:17:22 -0500

    Update L1_text2speech.py

--- software/python/basics/L1_text2speech.py
+++ software/python/basics/L1_text2speech.py
@@ -1,6 +1,7 @@
 # This program takes a message and sends it as a voice to the audio output.
 # Please run "sudo apt-get install flite" before running this program.
 # Next, you can adust volume with a GUI by the command "alsamixer"
+# Make sure that the usb is connected on boot to ensure the usb device is recognized
 
 import os # enables usage of shell commands
 
