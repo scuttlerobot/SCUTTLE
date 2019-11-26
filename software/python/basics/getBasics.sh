e6a4df75f370602b86c5cda3305395e831b4464b
dmalawey <42623372+dmalawey@users.noreply.github.com>
2019-11-26 15:18:52 -0600

    Create getBasics.sh

---
+++ software/python/basics/getBasics.sh
@@ -0,0 +1,28 @@
+# This script has shell commands to get most of the basics folder items from github.
+
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L0_mjpg_streamer_filter.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L1_adc.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L1_bmp.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L1_camera.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L1_encoder.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L1_gamepad.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L1_gpio.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L1_lidar.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L1_motors.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L1_mpu.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L1_mqtt.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L1_rfid.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L1_rssi.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L1_servo.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L1_text2speech.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L2_color_target.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L2_heading.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L2_inverse_kinematics.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L2_joint.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L2_kinematics.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L2_log.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L2_obstacle.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L2_speed_control.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L2_track_target.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L2_vector.py
+wget https://raw.githubusercontent.com/MXET/SCUTTLE/master/software/python/basics/L3_telemetry.py
