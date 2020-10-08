de1e13759dacfc0a3c98443021b2c36c04823561
dmalawey <42623372+dmalawey@users.noreply.github.com>
2020-10-08 18:15:21 +0800

    Update L1_servo.py

--- software/python/basics/L1_servo.py
+++ software/python/basics/L1_servo.py
@@ -2,54 +2,45 @@
 # servo position is specified by "duty."  If the servo is a continuous
 # type, the duty will set the speed instead of the position. 
 # Uses rcpy library.  Documentation: guitar.ucsd.edu/rcpy/rcpy.pdf
-# PROGRAM REQUIRES SUDO.
+# PROGRAM REQUIRES SUDO. Last udpated 2020.10.08
 
+# Import external libraries
 import time, math
 import getopt, sys
 import rcpy  # This automatically initizalizes the robotics cape
 import rcpy.servo as servo
 import rcpy.clock as clock	# For PWM period for servos
 
-# defaults
-duty = 1.5	# Duty cycle (-1.5,1.5)
-period = 0.02	# Set servo period to 20ms
-ch1 = 1	# Which channel (1-8), 0 outputs to all channels
-ch2 = 2
-ch3 = 3
+# INITIALIZE DEFAULT VARS
+duty = 1.5		# Duty cycle (-1.5,1.5 is the typical range)
+period = 0.02 	# recommended servo period: 20ms (this is the interval of commands)
+ch1 = 1			# select channel (1 thru 8 are available)
+ch2 = 2			# selection of 0 performs output on all channels
 
 rcpy.set_state(rcpy.RUNNING) # set state to rcpy.RUNNING
 srvo1 = servo.Servo(ch1)	# Create servo object
 srvo2 = servo.Servo(ch2)
-srvo3 = servo.Servo(ch3)
 clck1 = clock.Clock(srvo1, period)	# Set PWM period for servos
 clck2 = clock.Clock(srvo2, period)
-clck3 = clock.Clock(srvo3, period)
 
-#try:
-servo.enable()		# Enables 6v rail
+servo.enable()		# Enables 6v rail on beaglebone blue
 clck1.start()		# Starts PWM
 clck2.start()
-clck3.start()
 
 def move1(angle):
 	srvo1.set(angle)
 	
 def move2(angle):
 	srvo2.set(angle)
-	
-def move3(angle):
-	srvo3.set(angle)
-	
-# UNCOMMENT THE SECTION BELOW TO RUN AS STANDALONE PROGRAM
-print("beginning servo loop")
-while rcpy.get_state() != rcpy.EXITING: 	# keep running
-		print("move 1.5")
-		move1(1.5)	# Set servo duty
-		move2(1.5)
-		move3(1.5)
-		time.sleep(2)
-		print("move -0.5")
-		move1(-1.5)
-		move2(-1.5)
-		move3(-1.5)
-		time.sleep(2)
+
+# THE SECTION BELOW WILL ONLY RUN IF L1_SERVO.PY IS CALLED DIRECTLY	 
+if __name__ == "__main__":
+	while True:
+		print("beginning servo loop")
+		while rcpy.get_state() != rcpy.EXITING: 	# keep running
+				print("move 1.5")
+				move1(1.5)	# Set servo duty (1.5 has no units, see library for details)
+				time.sleep(2)
+				print("move -1.5")
+				move1(-1.5)
+				time.sleep(2)
