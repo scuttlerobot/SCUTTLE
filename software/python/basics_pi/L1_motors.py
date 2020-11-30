f2c024666483aea1afb0f7cc381352711074a4a6
DM <dmalawey@gmail.com>
2020-11-30 08:40:56 +0000

    improved motor PWM calculation

--- software/python/basics_pi/L1_motors.py
+++ software/python/basics_pi/L1_motors.py
@@ -1,53 +1,50 @@
 # Motors Program for SCUTTLE running RasPi
 # This example sends commands to two motors on the appropriate pins for H-bridge
 # For pin mapping, see Wiring Guide Pi on the SCUTTLE webpage.
-# Last update: 2020.11
+# Last update: 2020.11 with improved PWM method
 
 # Import external libraries
-import gpiozero                         # used for PWM outputs
-from gpiozero import PWMOutputDevice
-import time
+import gpiozero                             # used for PWM outputs
+from gpiozero import PWMOutputDevice as pwm # for driving motors, LEDs, etc
+import time                                 # for keeping time
+import numpy as np                          # for handling arrays
 
-# Info on pins:
-# Broadcom (BCM) pin numbering for Pi gives names of GPIO17
-# and GPIO18 to physical pins 11 and 12
-leftOutA = gpio.PWMOutputDevice(17, frequency=1000,initial_value=0)
-leftOutB = gpio.PWMOutputDevice(18, frequency=1000,initial_value=0)
+# Broadcom (BCM) pin numbering for RasPi is as follows: PHYSICAL:       NAME:
+left_chA  = pwm(17, frequency=5000,initial_value=0)     # PIN 11        GPIO17
+left_chB  = pwm(18, frequency=5000,initial_value=0)     # PIN 12        GPIO18
+right_chA = pwm(22, frequency=5000,initial_value=0)     # PIN 15        GPIO22
+right_chB = pwm(23, frequency=5000,initial_value=0)     # PIN 16        GPIO23
 
-RightOutA = gpio.PWMOutputDevice(22, frequency=1000,initial_value=0)
-RightOutB = gpio.PWMOutputDevice(23, frequency=1000,initial_value=0)
+def computePWM(speed):          # take an argument in range [-1,1]
+    x = speed + 1.0             # change the range to [0,2]
+    chA = 0.5 * x               # channel A sweeps low to high
+    chB = 1 - (0.5 * x)         # channel B sweeps high to low
+    x = np.array([chA, chB])    # store values to an array
+    return(x)
 
-# Channel refers to left(0) or right(1)
-def MotorL(speed):
-    if speed>0:
-        leftOutB.value = speed 
-        leftOutA.value = 0
-    elif speed<0:
-        leftOutB.value = 0
-        leftOutA.value = (-1*speed) #drive opposite polarity with positive duty cycle
-    elif speed==0:
-        leftOutB.value = 0
-        leftOutA.value = 0
+def sendLeft(mySpeed):          # takes at least 0.3 ms
+    myPWM = computePWM(mySpeed)
+    left_chA.value = myPWM[0]
+    left_chB.value = myPWM[1]
 
-def MotorR(speed):
-    if speed>0:
-        RightOutB.value = speed
-        RightOutA.value = 0
-    elif speed<0:
-        RightOutB.value = 0
-        RightOutA.value = (-1*speed) #drive opposite polarity with positive duty cycle
-    elif speed==0:
-        RightOutB.value = 0
-        RightOutA.value = 0
+def sendRight(mySpeed):         # takes at least 0.3 ms
+    myPWM = computePWM(mySpeed)
+    right_chA.value = myPWM[0]
+    right_chB.value = myPWM[1]
 
 # THIS LOOP ONLY RUNS IF THE PROGRAM IS CALLED DIRECTLY
 if __name__ == "__main__":
+    myRate = 0.
     while(1):
         print("motors.py: driving fwd")
-        MotorL(0.6)                         # gentle speed for testing program. 0.3 PWM may not spin the wheels.
-        MotorR(0.6)
+        sendLeft(0.8)
+        sendRight(0.8)
         time.sleep(4)                       # run fwd for 4 seconds
         print("motors.py: driving reverse")
-        MotorL(-0.6)
-        MotorR(-0.6)
+        sendLeft(-0.8)
+        sendRight(-0.8)
         time.sleep(4)                       # run reverse for 4 seconds
+        print("stopping motors 4 seconds")
+        sendLeft(0)
+        sendRight(0)
+        time.sleep(4)
