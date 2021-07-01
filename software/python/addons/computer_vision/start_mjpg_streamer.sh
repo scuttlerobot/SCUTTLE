d5f6d9f7a637518c32490d78e018247bf5060d3c
Daniyal Ansari <daniyal.s.ansari+github@gmail.com>
2021-06-30 20:02:15 -0500

    mjpg streamer

--- software/python/addons/computer_vision/start_mjpg_streamer.sh
+++ software/python/addons/computer_vision/start_mjpg_streamer.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+touch /tmp/h_min
+touch /tmp/h_max
+touch /tmp/s_min
+touch /tmp/s_max
+touch /tmp/v_min
+touch /tmp/v_max
+
 /usr/local/bin/mjpg_streamer -i "/usr/local/lib/mjpg-streamer/input_opencv.so --filter /usr/local/lib/mjpg-streamer/cvfilter_py.so --fargs ./L3_image_filter.py" -o "/usr/local/lib/mjpg-streamer/output_http.so -p 8090 -w /usr/local/share/mjpg-streamer/www"
 
