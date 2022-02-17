78626e3c3efce89eb35122882b30ca0d7d013c8c
David <42623372+dmalawey@users.noreply.github.com>
2022-02-17 11:39:32 -0600

    Update L3_follow.py

--- software/python/basics_pi/L3_follow.py
+++ software/python/basics_pi/L3_follow.py
@@ -28,7 +28,7 @@ def forwardFunction(r0):            # when the target is straight on, approach
 def turnAndGo(xVal):                                            # turn towards object and approach
     xVal = 0.8* xVal                                            # scale down for slower turns
     centerBand = 0.15                                           # in this band, don't turn
-    if abs(x_range) > centerBand:                               # outside the band, make a turn
+    if abs(x_offset) > centerBand:                              # if x_offset is outside the band, make a turn
         chassisTargets = inv.map_speeds(np.array([0, xVal]))    # generate xd, td
     else: 
         xdt = forwardFunction(radius)                           # determine forward speed
@@ -42,10 +42,10 @@ if __name__ == "__main__":
         if target[0] is None:                                   # if there is no colored target detected
             print("no target located.")                         # do not drive
         else:
-            x_range = round(track.getAngle(target[0]),2)        # find out the angle of the target
+            x_offset = round(track.getAngle(target[0]),2)        # find out the angle of the target
             radius = round(target[2],1)                         # find out the radius of the target
-            print("Target position: ", x_range, "\t radius", radius)
-            chassisTargets = turnAndGo(x_range)                 # take the x target location & generate turning
+            print("Target position: ", x_offset, "\t radius", radius)
+            chassisTargets = turnAndGo(x_offset)                 # take the x target location & generate turning
             pdTargets = inv.convert(chassisTargets)             # phi dot targets (rad/s)
             sc.driveOpenLoop(pdTargets)                         # command motors in open-loop fashion
         time.sleep(0.01)                                        # short delay
