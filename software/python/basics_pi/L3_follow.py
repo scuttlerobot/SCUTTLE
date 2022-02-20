4d7e87e8762cd7ac5885b60a99057a1f105b10ce
David <42623372+dmalawey@users.noreply.github.com>
2022-02-20 15:33:19 -0600

    Update L3_follow.py

--- software/python/basics_pi/L3_follow.py
+++ software/python/basics_pi/L3_follow.py
@@ -14,9 +14,15 @@ import L2_inverse_kinematics as inv
 # Declare relevant variables
 radius = 0                          # radius of target
 cruiseRate = 0.6                    # speed for cruising (fraction)
+turnRate = 0.8                      # speed for turning (fraction)
 r1 = 28                             # radius desired (pixels)
 tol = 3                             # radius tolerance (pixels)
-centerBand = 0.18                   # portion of FOV to consider target centered
+centerBand = 0.20                   # portion of FOV to consider target centered
+
+# Declare variables for time-keeping
+t1 = time.monotonic()  # declare vars
+t0 = t1              # for keeping time
+print("initial time: ", t1)
 
 def forwardFunction(r0):            # when the target is straight on, approach          
     xdt = 0                         # initial x_dot target is zero
@@ -26,25 +32,31 @@ def forwardFunction(r0):            # when the target is straight on, approach
         xdt = -1* cruiseRate        # x_dot target, drive backwards
     return xdt
 
-def turnAndGo(xVal):                                            # turn towards object and approach
-    if abs(x_offset) > centerBand:                              # if x_offset is outside the band, make a turn
-        chassisTargets = inv.map_speeds(np.array([0, x_offset]))    # generate xd, td
+def turnAndGo(xVal):                                           # turn towards object and approach
+    if abs(x_offset) > centerBand:                             # if x_offset is outside the band, make a turn
+        pdt = np.sign(x_offset) * x_offset * x_offset * turnRate # square the offset, keep the sign, & scale by constant
+        chassisTargets = inv.map_speeds(np.array([0, pdt]))    # generate xd, td
     else: 
-        xdt = forwardFunction(radius)                           # determine forward speed
-        chassisTargets = inv.map_speeds(np.array([xdt,0]))      # set td zero
+        xdt = forwardFunction(radius)                          # determine forward speed
+        chassisTargets = inv.map_speeds(np.array([xdt,0]))     # set td zero
     return chassisTargets
 
 # THIS SECTION ONLY RUNS IF THE PROGRAM IS CALLED DIRECTLY
 if __name__ == "__main__":
+    
     while True:
         target = track.colorTarget(track.color_range)           # generate a target
+        t1 = time.monotonic()                                   # measure loop end time
+        t = round(t1 - t0, 3)                                   # compute loop in seconds
+        t0 = t1                                                 # reset loop base
+
         if target[0] is None:                                   # if there is no colored target detected
-            print("no target located.")                         # do not drive
+            print("interval: ", t, "\tno target located.")      # do not drive
         else:
             x_offset = round(track.getAngle(target[0]),2)       # find angle of the target's x_offset (% of FOV)
-            radius = round(target[2],1)                         # find out the radius of the target
-            print("Target position: ", x_offset, "\t radius", radius)
-            chassisTargets = turnAndGo(x_offset)                 # take the x target location & generate turning
+            radius = int(target[2])                             # find out the radius of the target
+            print("interval: ", t, "\t", "Target position: ", x_offset, "\t radius", radius)
+            chassisTargets = turnAndGo(x_offset)                # take the x target location & generate turning
             pdTargets = inv.convert(chassisTargets)             # phi dot targets (rad/s)
             sc.driveOpenLoop(pdTargets)                         # command motors in open-loop fashion
-        time.sleep(0.01)                                        # short delay
+            time.sleep(0.07)                                        # execution = sleep + 30ms on pi 4B+
